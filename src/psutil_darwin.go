d4a4defb02077f62316195a0c52d0e27d2c29109
Pavlo Golub <pavlo.golub@gmail.com>
2022-05-02 15:48:25 +0200

    add Darwin platform

---
+++ src/psutil_darwin.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+	"syscall"
+)
+
+func getPathUnderlyingDeviceId(path string) (uint64, error) {
+	fp, err := os.Open(path)
+	if err != nil {
+		return 0, err
+	}
+	fi, err := fp.Stat()
+	if err != nil {
+		return 0, err
+	}
+	stat := fi.Sys().(*syscall.Stat_t)
+	return stat.Dev, nil
+}
