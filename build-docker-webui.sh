cfa73065e3b18afcf378cde77fcc86d3a081d1ad
Pavlo Golub <pavlo.golub@gmail.com>
2022-12-23 09:49:16 +0100

    remove old webui docker stuff

--- build-docker-webui.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker build --no-cache \
- --build-arg GIT_TIME=`git show -s --format=%cI HEAD` \
- --build-arg GIT_HASH=`git show -s --format=%H HEAD` \
- -t cybertec/pgwatch3-webui:latest \
- -f docker/Dockerfile-webui .
