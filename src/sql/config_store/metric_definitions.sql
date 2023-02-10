22237c31fbfa60a2c43092214791b9f9f2f7c5d1
Pavlo Golub <pavlo.golub@gmail.com>
2023-02-10 14:30:44 +0100

    bump `table_io_stats` metric

--- src/sql/config_store/metric_definitions.sql
+++ src/sql/config_store/metric_definitions.sql
@@ -666,47 +666,10 @@ $sql$
 );
 
 /* table_io_stats */
-
 insert into pgwatch3.metric(m_name, m_pg_version_from, m_sql)
 values (
 'table_io_stats',
-9.0,
-$sql$
-SELECT
-  (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
-  schemaname::text as tag_schema,
-  relname::text as tag_table_name,
-  quote_ident(schemaname)||'.'||quote_ident(relname) as tag_table_full_name,
-  heap_blks_read,
-  heap_blks_hit,
-  idx_blks_read,
-  idx_blks_hit,
-  toast_blks_read,
-  toast_blks_hit,
-  tidx_blks_read,
-  tidx_blks_hit
-FROM
-  pg_statio_user_tables
-WHERE
-  NOT schemaname LIKE E'pg\\_temp%'
-  AND (heap_blks_read > 0 OR heap_blks_hit > 0 OR idx_blks_read > 0 OR idx_blks_hit > 0 OR tidx_blks_read > 0 OR tidx_blks_hit > 0)
-ORDER BY
-  coalesce(heap_blks_read, 0) +
-  coalesce(heap_blks_hit, 0) +
-  coalesce(idx_blks_read, 0) +
-  coalesce(idx_blks_hit, 0) +
-  coalesce(toast_blks_read, 0) +
-  coalesce(toast_blks_hit, 0) +
-  coalesce(tidx_blks_read, 0) +
-  coalesce(tidx_blks_hit, 0)
-  DESC LIMIT 300;
-$sql$
-);
-
-insert into pgwatch3.metric(m_name, m_pg_version_from, m_sql)
-values (
-'table_io_stats',
-10,
+11,
 $sql$
 select * from (
                   with recursive
