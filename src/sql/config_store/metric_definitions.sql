11521f13c5a680d6841c7ad36f3b8918577e6214
Pavlo Golub <pavlo.golub@gmail.com>
2023-02-10 14:49:45 +0100

    bump all change events metrics

--- src/sql/config_store/metric_definitions.sql
+++ src/sql/config_store/metric_definitions.sql
@@ -3142,7 +3142,7 @@ $sql$
 insert into pgwatch3.metric(m_name, m_pg_version_from,m_sql)
 values (
 'change_events',
-9.0,
+11,
 $sql$
 $sql$
 );
@@ -3151,7 +3151,7 @@ $sql$
 insert into pgwatch3.metric(m_name, m_pg_version_from,m_sql)
 values (
 'sproc_hashes',
-9.0,
+11,
 $sql$
 select
   (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
@@ -3171,72 +3171,7 @@ $sql$
 insert into pgwatch3.metric(m_name, m_pg_version_from, m_sql)
 values (
 'table_hashes',
-9.0,
-$sql$
-select
-  (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
-  quote_ident(table_schema)||'.'||quote_ident(table_name) as tag_table,
-  md5((array_agg((c.*)::text order by ordinal_position))::text)
-from (
-         SELECT current_database()::information_schema.sql_identifier AS table_catalog, nc.nspname::information_schema.sql_identifier AS table_schema, c.relname::information_schema.sql_identifier AS table_name, a.attname::information_schema.sql_identifier AS column_name, a.attnum::information_schema.cardinal_number AS ordinal_position, pg_get_expr(ad.adbin, ad.adrelid)::information_schema.character_data AS column_default,
-                CASE
-                    WHEN a.attnotnull OR t.typtype = 'd'::"char" AND t.typnotnull THEN 'NO'::text
-                    ELSE 'YES'::text
-                    END::information_schema.yes_or_no AS is_nullable,
-                CASE
-                    WHEN t.typtype = 'd'::"char" THEN
-                        CASE
-                            WHEN bt.typelem <> 0::oid AND bt.typlen = (-1) THEN 'ARRAY'::text
-                            WHEN nbt.nspname = 'pg_catalog'::name THEN format_type(t.typbasetype, NULL::integer)
-                            ELSE 'USER-DEFINED'::text
-                            END
-                    ELSE
-                        CASE
-                            WHEN t.typelem <> 0::oid AND t.typlen = (-1) THEN 'ARRAY'::text
-                            WHEN nt.nspname = 'pg_catalog'::name THEN format_type(a.atttypid, NULL::integer)
-                            ELSE 'USER-DEFINED'::text
-                            END
-                    END::information_schema.character_data AS data_type, information_schema._pg_char_max_length(information_schema._pg_truetypid(a.*, t.*), information_schema._pg_truetypmod(a.*, t.*))::information_schema.cardinal_number AS character_maximum_length, information_schema._pg_char_octet_length(information_schema._pg_truetypid(a.*, t.*), information_schema._pg_truetypmod(a.*, t.*))::information_schema.cardinal_number AS character_octet_length, information_schema._pg_numeric_precision(information_schema._pg_truetypid(a.*, t.*), information_schema._pg_truetypmod(a.*, t.*))::information_schema.cardinal_number AS numeric_precision, information_schema._pg_numeric_precision_radix(information_schema._pg_truetypid(a.*, t.*), information_schema._pg_truetypmod(a.*, t.*))::information_schema.cardinal_number AS numeric_precision_radix, information_schema._pg_numeric_scale(information_schema._pg_truetypid(a.*, t.*), information_schema._pg_truetypmod(a.*, t.*))::information_schema.cardinal_number AS numeric_scale, information_schema._pg_datetime_precision(information_schema._pg_truetypid(a.*, t.*), information_schema._pg_truetypmod(a.*, t.*))::information_schema.cardinal_number AS datetime_precision, NULL::character varying::information_schema.character_data AS interval_type, NULL::character varying::information_schema.character_data AS interval_precision, NULL::character varying::information_schema.sql_identifier AS character_set_catalog, NULL::character varying::information_schema.sql_identifier AS character_set_schema, NULL::character varying::information_schema.sql_identifier AS character_set_name, NULL::character varying::information_schema.sql_identifier AS collation_catalog, NULL::character varying::information_schema.sql_identifier AS collation_schema, NULL::character varying::information_schema.sql_identifier AS collation_name,
-                CASE
-                    WHEN t.typtype = 'd'::"char" THEN current_database()
-                    ELSE NULL::name
-                    END::information_schema.sql_identifier AS domain_catalog,
-                CASE
-                    WHEN t.typtype = 'd'::"char" THEN nt.nspname
-                    ELSE NULL::name
-                    END::information_schema.sql_identifier AS domain_schema,
-                CASE
-                    WHEN t.typtype = 'd'::"char" THEN t.typname
-                    ELSE NULL::name
-                    END::information_schema.sql_identifier AS domain_name, current_database()::information_schema.sql_identifier AS udt_catalog, COALESCE(nbt.nspname, nt.nspname)::information_schema.sql_identifier AS udt_schema, COALESCE(bt.typname, t.typname)::information_schema.sql_identifier AS udt_name, NULL::character varying::information_schema.sql_identifier AS scope_catalog, NULL::character varying::information_schema.sql_identifier AS scope_schema, NULL::character varying::information_schema.sql_identifier AS scope_name, NULL::integer::information_schema.cardinal_number AS maximum_cardinality, a.attnum::information_schema.sql_identifier AS dtd_identifier, 'NO'::character varying::information_schema.yes_or_no AS is_self_referencing, 'NO'::character varying::information_schema.yes_or_no AS is_identity, NULL::character varying::information_schema.character_data AS identity_generation, NULL::character varying::information_schema.character_data AS identity_start, NULL::character varying::information_schema.character_data AS identity_increment, NULL::character varying::information_schema.character_data AS identity_maximum, NULL::character varying::information_schema.character_data AS identity_minimum, NULL::character varying::information_schema.yes_or_no AS identity_cycle, 'NEVER'::character varying::information_schema.character_data AS is_generated, NULL::character varying::information_schema.character_data AS generation_expression,
-                CASE
-                    WHEN c.relkind = 'r'::"char" OR c.relkind = 'v'::"char" AND (EXISTS ( SELECT 1
-                                                                                          FROM pg_rewrite
-                                                                                          WHERE pg_rewrite.ev_class = c.oid AND pg_rewrite.ev_type = '2'::"char" AND pg_rewrite.is_instead)) AND (EXISTS ( SELECT 1
-                                                                                                                                                                                                           FROM pg_rewrite
-                                                                                                                                                                                                           WHERE pg_rewrite.ev_class = c.oid AND pg_rewrite.ev_type = '4'::"char" AND pg_rewrite.is_instead)) THEN 'YES'::text
-                    ELSE 'NO'::text
-                    END::information_schema.yes_or_no AS is_updatable
-         FROM pg_attribute a
-                  LEFT JOIN pg_attrdef ad ON a.attrelid = ad.adrelid AND a.attnum = ad.adnum, pg_class c, pg_namespace nc, pg_type t
-                                                                                                                               JOIN pg_namespace nt ON t.typnamespace = nt.oid
-                                                                                                                               LEFT JOIN (pg_type bt
-             JOIN pg_namespace nbt ON bt.typnamespace = nbt.oid) ON t.typtype = 'd'::"char" AND t.typbasetype = bt.oid
-         WHERE a.attrelid = c.oid AND a.atttypid = t.oid AND nc.oid = c.relnamespace AND NOT pg_is_other_temp_schema(nc.oid) AND a.attnum > 0 AND NOT a.attisdropped AND (c.relkind = ANY (ARRAY['r'::"char", 'v'::"char"])) AND (pg_has_role(c.relowner, 'USAGE'::text) OR has_column_privilege(c.oid, a.attnum, 'SELECT, INSERT, UPDATE, REFERENCES'::text))
-) c
-where
-  not table_schema like any (array[E'pg\\_%', 'information_schema'])
-group by
-  table_schema, table_name
-order by
-  table_schema, table_name;
-$sql$
-);
-
-insert into pgwatch3.metric(m_name, m_pg_version_from, m_sql)
-values (
-'table_hashes',
-9.3,
+11,
 $sql$
 select
   (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
@@ -3345,7 +3280,7 @@ $sql$
 insert into pgwatch3.metric(m_name, m_pg_version_from,m_sql)
 values (
 'configuration_hashes',
-9.0,
+11,
 $sql$
 select
   (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
@@ -3360,7 +3295,7 @@ $sql$
 insert into pgwatch3.metric(m_name, m_pg_version_from,m_sql)
 values (
 'index_hashes',
-9.0,
+11,
 $sql$
 select
   (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
@@ -3385,7 +3320,7 @@ $sql$
 insert into pgwatch3.metric(m_name, m_pg_version_from, m_sql, m_comment)
 values (
 'privilege_changes',
-9.0,
+11,
 $sql$
 SELECT
     (extract(epoch FROM now()) * 1e9)::int8 AS epoch_ns,
@@ -3409,37 +3344,7 @@ FROM (
                 rolsuper
                 OR oid < 16384)
             AND NOT table_schema IN ('information_schema', 'pg_catalog')
-            /*
-             union all
 
-             select
-             -- quite a heavy query currently, maybe faster directly via pg_attribute + has_column_privilege?
-            'column' AS object_type,
-            grantee::text AS tag_role,
-            quote_ident(table_schema) || '.' || quote_ident(table_name) AS tag_object,
-            privilege_type
-        FROM
-            information_schema.column_privileges cp
-        WHERE
-            NOT table_schema IN ('pg_catalog', 'information_schema')
-            AND NOT grantee = ANY (
-                SELECT
-                    rolname
-                FROM
-                    pg_roles
-                WHERE
-                    rolsuper
-                    OR oid < 16384)
-                AND NOT EXISTS (
-                    SELECT
-                        *
-                    FROM
-                        information_schema.table_privileges
-                    WHERE
-                        table_schema = cp.table_schema
-                        AND table_name = cp.table_name
-                        AND grantee = cp.grantee
-                        AND privilege_type = cp.privilege_type) */
                 UNION ALL
                 SELECT
                     'function' AS object_type,
@@ -3523,7 +3428,7 @@ $sql$,
 insert into pgwatch3.metric(m_name, m_pg_version_from, m_sql, m_comment, m_is_helper)
 values (
 'get_load_average',
-9.1,
+11,
 $sql$
 BEGIN;
 
@@ -3550,7 +3455,7 @@ true
 insert into pgwatch3.metric(m_name, m_pg_version_from, m_sql, m_comment, m_is_helper)
 values (
 'get_load_average_copy',
-9.0,
+11,
 $sql$
 BEGIN;
 
@@ -3614,7 +3519,7 @@ true
 insert into pgwatch3.metric(m_name, m_pg_version_from, m_sql, m_comment, m_is_helper)
 values (
 'get_load_average_windows',
-9.0,
+11,
 $sql$
 /*
 
