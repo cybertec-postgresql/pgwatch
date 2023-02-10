593d1a804e247e0bfe6741c5f53ef3d94be7d1f7
Denys2002Holub <paloma20022001@gmail.com>
2023-02-10 14:10:37 +0100

    Added enabled column

--- src/webui/src/layout/MetricDefinitions/MetricsTable/metricsTableColumns.tsx
+++ src/webui/src/layout/MetricDefinitions/MetricsTable/metricsTableColumns.tsx
@@ -1,4 +1,6 @@
-import { GridColDef } from "@mui/x-data-grid";
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
+import { GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 
 export const columns: GridColDef[] = [
     {
@@ -15,13 +17,22 @@ export const columns: GridColDef[] = [
         align: "center",
         headerAlign: "center",
         valueGetter(params) {
-            return(Object.keys(params.value));
+            return (Object.keys(params.value));
         },
     },
     {
         field: "m_comment",
         headerName: "Comment",
         width: 150,
+        align: "center",
+        headerAlign: "center"
+    },
+    {
+        field: "m_is_active",
+        headerName: "Enabled?",
+        width: 120,
+        type: "boolean",
+        renderCell: (params: GridRenderCellParams<boolean>) => params.value ? <CheckIcon /> : <CloseIcon />,
         align: "center",
         headerAlign: "center"
     }
