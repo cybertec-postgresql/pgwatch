108349a37be24c046c7e8095e9383597d1ab7d1f
Denys2002Holub <paloma20022001@gmail.com>
2023-02-10 14:16:27 +0100

    Added modified column

--- src/webui/src/layout/MetricDefinitions/MetricsTable/metricsTableColumns.tsx
+++ src/webui/src/layout/MetricDefinitions/MetricsTable/metricsTableColumns.tsx
@@ -1,6 +1,8 @@
 import CheckIcon from '@mui/icons-material/Check';
 import CloseIcon from '@mui/icons-material/Close';
+import { Box } from '@mui/material';
 import { GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
+import moment from 'moment';
 
 export const columns: GridColDef[] = [
     {
@@ -40,6 +42,17 @@ export const columns: GridColDef[] = [
         headerName: "Helpers?",
         width: 120,
         renderCell: (params: GridRenderCellParams<boolean>) => params.value ? <CheckIcon /> : <CloseIcon />,
+        align: "center",
+        headerAlign: "center"
+    },
+    {
+        field: "m_last_modified_on",
+        headerName: "Last modified",
+        width: 150,
+        renderCell: (params) =>
+            <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
+                {moment(params.value).format("MMMM Do YYYY HH:mm:ss")}
+            </Box>,
         align: "center",
         headerAlign: "center"
     }
