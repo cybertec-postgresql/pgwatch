6bb0b3494e8101db3cf1b4f74ab452496465f3b7
Denys2002Holub <paloma20022001@gmail.com>
2023-02-14 12:30:33 +0100

    changed location of columns definition

--- src/webui/src/layout/MetricDefinitions/MetricsTable/index.tsx
+++ src/webui/src/layout/MetricDefinitions/MetricsTable/index.tsx
@@ -1,8 +1,10 @@
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
 import { Box, Typography } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
+import moment from 'moment';
+import { ActionsComponent } from './ActionsComponent';
 import { GridToolbarComponent } from "./GridToolbarComponent";
-import { columns } from "./metricsTableColumns";
-
 
 export const MetricsTable = () => {
 
@@ -32,6 +34,70 @@ export const MetricsTable = () => {
       "m_is_active": false,
       "m_is_helper": false,
       "m_last_modified_on": Date.now()
+    }
+  ];
+
+  const columns: GridColDef[] = [
+    {
+      field: "m_name",
+      headerName: "Metric name",
+      width: 150,
+      align: "center",
+      headerAlign: "center"
+    },
+    {
+      field: "m_versions",
+      headerName: "PG versions",
+      width: 150,
+      align: "center",
+      headerAlign: "center",
+      valueGetter(params) {
+        return (Object.keys(params.value));
+      },
+    },
+    {
+      field: "m_comment",
+      headerName: "Comment",
+      width: 150,
+      align: "center",
+      headerAlign: "center"
+    },
+    {
+      field: "m_is_active",
+      headerName: "Enabled?",
+      width: 120,
+      renderCell: (params: GridRenderCellParams<boolean>) => params.value ? <CheckIcon /> : <CloseIcon />,
+      align: "center",
+      headerAlign: "center"
+    },
+    {
+      field: "m_is_helper",
+      headerName: "Helpers?",
+      width: 120,
+      renderCell: (params: GridRenderCellParams<boolean>) => params.value ? <CheckIcon /> : <CloseIcon />,
+      align: "center",
+      headerAlign: "center"
+    },
+    {
+      field: "m_last_modified_on",
+      headerName: "Last modified",
+      width: 150,
+      renderCell: (params) =>
+        <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
+          {moment(params.value).format("MMMM Do YYYY HH:mm:ss")}
+        </Box>,
+      align: "center",
+      headerAlign: "center"
+    },
+    {
+      field: "actions",
+      headerName: "Actions",
+      type: "actions",
+      width: 350,
+      renderCell: () => (
+        <ActionsComponent />
+      ),
+      headerAlign: "center"
     }
   ];
 
