58fe65d4d17adc7480c4fe54daa9d54f2b5d4ecb
Denys2002Holub <paloma20022001@gmail.com>
2023-02-14 12:39:53 +0100

    removed sample data

--- src/webui/src/layout/MetricDefinitions/MetricsTable/index.tsx
+++ src/webui/src/layout/MetricDefinitions/MetricsTable/index.tsx
@@ -8,35 +8,6 @@ import { GridToolbarComponent } from "./GridToolbarComponent";
 
 export const MetricsTable = () => {
 
-  const data = [
-    {
-      "m_name": "backends",
-      "m_versions": {
-        "9.0": "sql",
-        "9.1": "sql",
-        "10.0": "sql",
-        "15.0": "sql"
-      },
-      "m_comment": "comment",
-      "m_is_active": false,
-      "m_is_helper": false,
-      "m_last_modified_on": Date.now()
-    },
-    {
-      "m_name": "wsl",
-      "m_versions": {
-        "9.3": "sql",
-        "9.5": "sql",
-        "11.2": "sql",
-        "14.7": "sql"
-      },
-      "m_comment": "comment",
-      "m_is_active": false,
-      "m_is_helper": false,
-      "m_last_modified_on": Date.now()
-    }
-  ];
-
   const columns: GridColDef[] = [
     {
       field: "m_name",
@@ -110,7 +81,7 @@ export const MetricsTable = () => {
         getRowHeight={() => "auto"}
         getRowId={(row) => row.m_name}
         columns={columns}
-        rows={data}
+        rows={[]}
         autoHeight
         pageSize={5}
         components={{ Toolbar: () => GridToolbarComponent() }}
