0840a8ad8d55e131d4a6a2627a8266537568efaf
Denys2002Holub <paloma20022001@gmail.com>
2023-02-17 12:20:58 +0100

    added alert for result of deleting. Implemented 'GET /metric' & 'DELETE /metric'. Changed icons to checkboxes in grid

--- src/webui/src/layout/MetricDefinitions/MetricsTable/ActionsComponent.tsx
+++ src/webui/src/layout/MetricDefinitions/MetricsTable/ActionsComponent.tsx
@@ -1,12 +1,32 @@
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
-import { Box, Button } from "@mui/material";
+import { AlertColor, Box, Button } from "@mui/material";
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { QueryKeys } from 'queries/queryKeys';
+import { Metric } from 'queries/types/MetricTypes';
+import MetricService from 'services/Metric';
 
-type Props = {
-  data: any
+type Params = {
+  data: Metric,
+  handleAlertOpen: (isOpen: boolean, text: string, type: AlertColor) => void
 };
 
-export const ActionsComponent = ({ data }: Props) => {
+export const ActionsComponent = ({ data, handleAlertOpen }: Params) => {
+  const services = MetricService.getInstance();
+  const queryClient = useQueryClient();
+
+  const deleteRecord = useMutation({
+    mutationFn: async (m_id: number) => {
+      return await services.deleteMetric(m_id);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: QueryKeys.metric });
+      handleAlertOpen(true, `Metric "${data.m_name}" has been deleted successfully!`, "success");
+    },
+    onError: (error: any) => {
+      handleAlertOpen(true, error.response.data, "error");
+    }
+  });
 
   const handleClick = () => {
     alert(JSON.stringify(data));
@@ -26,6 +46,7 @@ export const ActionsComponent = ({ data }: Props) => {
         size="small"
         variant="contained"
         startIcon={<DeleteIcon />}
+        onClick={() => deleteRecord.mutate(data.m_id)}
       >
         Delete
       </Button>
--- src/webui/src/layout/MetricDefinitions/MetricsTable/index.tsx
+++ src/webui/src/layout/MetricDefinitions/MetricsTable/index.tsx
@@ -1,33 +1,36 @@
-import CheckIcon from '@mui/icons-material/Check';
-import CloseIcon from '@mui/icons-material/Close';
-import { Box, Typography } from "@mui/material";
+import { useState } from "react";
+import { Alert, AlertColor, Box, Checkbox, CircularProgress, Snackbar, Typography } from "@mui/material";
 import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
+import { useQuery } from '@tanstack/react-query';
 import moment from 'moment';
+import { QueryKeys } from 'queries/queryKeys';
+import { Metric } from 'queries/types/MetricTypes';
+import MetricService from 'services/Metric';
 import { ActionsComponent } from './ActionsComponent';
 import { GridToolbarComponent } from "./GridToolbarComponent";
 
 export const MetricsTable = () => {
+  const services = MetricService.getInstance();
+  const [alertOpen, setAlertOpen] = useState(false);
+  const [severity, setSeverity] = useState<AlertColor>();
+  const [alertText, setAlertText] = useState("");
 
-  const data = [
-    {
-      "m_id": 1,
-      "m_name": "wsl",
-      "m_version": 14,
-      "m_comment": "some comment about metric",
-      "m_is_active": true,
-      "m_is_helper": false,
-      "m_last_modified_on": new Date()
-    },
-    {
-      "m_id": 2,
-      "m_name": "backends",
-      "m_version": 10.0,
-      "m_comment": "some comment about metric",
-      "m_is_active": true,
-      "m_is_helper": false,
-      "m_last_modified_on": new Date()
+  const handleAlertOpen = (isOpen: boolean, text: string, type: AlertColor) => {
+    setSeverity(type);
+    setAlertText(text);
+    setAlertOpen(isOpen);
+  };
+
+  const handleAlertClose = () => {
+    setAlertOpen(false);
+  };
+
+  const { status, data } = useQuery<Metric[]>({
+    queryKey: QueryKeys.metric,
+    queryFn: async () => {
+      return await services.getMetrics();
     }
-  ];
+  });
 
   const columns: GridColDef[] = [
     {
@@ -47,7 +50,7 @@ export const MetricsTable = () => {
       headerAlign: "center"
     },
     {
-      field: "m_version",
+      field: "m_pg_version_from",
       headerName: "PG version",
       width: 150,
       type: "number",
@@ -57,7 +60,7 @@ export const MetricsTable = () => {
     {
       field: "m_comment",
       headerName: "Comment",
-      width: 150,
+      width: 200,
       align: "center",
       headerAlign: "center"
     },
@@ -65,7 +68,7 @@ export const MetricsTable = () => {
       field: "m_is_active",
       headerName: "Enabled?",
       width: 120,
-      renderCell: (params: GridRenderCellParams<boolean>) => params.value ? <CheckIcon /> : <CloseIcon />,
+      renderCell: (params: GridRenderCellParams<boolean>) => <Checkbox checked={params.value} disableRipple />,
       align: "center",
       headerAlign: "center"
     },
@@ -73,7 +76,23 @@ export const MetricsTable = () => {
       field: "m_is_helper",
       headerName: "Helpers?",
       width: 120,
-      renderCell: (params: GridRenderCellParams<boolean>) => params.value ? <CheckIcon /> : <CloseIcon />,
+      renderCell: (params: GridRenderCellParams<boolean>) => <Checkbox checked={params.value} disableRipple />,
+      align: "center",
+      headerAlign: "center"
+    },
+    {
+      field: "m_master_only",
+      headerName: "Master only?",
+      width: 120,
+      renderCell: (params: GridRenderCellParams<boolean>) => <Checkbox checked={params.value} disableRipple />,
+      align: "center",
+      headerAlign: "center"
+    },
+    {
+      field: "m_standby_only",
+      headerName: "Standby only?",
+      width: 120,
+      renderCell: (params: GridRenderCellParams<boolean>) => <Checkbox checked={params.value} disableRipple />,
       align: "center",
       headerAlign: "center"
     },
@@ -94,14 +113,33 @@ export const MetricsTable = () => {
       type: "actions",
       width: 200,
       renderCell: (params) => (
-        <ActionsComponent data={params.row} />
+        <ActionsComponent data={params.row} handleAlertOpen={handleAlertOpen} />
       ),
       headerAlign: "center"
     }
   ];
 
+  if (status === "loading") {
+    return (
+      <Box sx={{ width: "100%", height: 500, display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <CircularProgress />
+      </Box>
+    );
+  };
+
+  if (status === "error") {
+    return (
+      <Box>
+        <Typography>Some error happens</Typography>
+      </Box>
+    );
+  };
+
   return (
     <Box display="flex" flexDirection="column" gap={1} height="100%">
+      <Snackbar open={alertOpen} autoHideDuration={5000} onClose={handleAlertClose}>
+        <Alert sx={{ width: "auto" }} variant="filled" severity={severity}>{alertText}</Alert>
+      </Snackbar>
       <Typography variant="h5">
         Metrics
       </Typography>
