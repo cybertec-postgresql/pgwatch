d14bcf7adee1a577ba4a3807c101fb73a28063c5
Denys2002Holub <paloma20022001@gmail.com>
2023-03-14 13:55:03 +0100

    renamed custom component

--- src/webui/src/layout/PresetConfigs/index.tsx
+++ src/webui/src/layout/PresetConfigs/index.tsx
@@ -1,11 +1,11 @@
 import { Box } from "@mui/material";
-import { ConfigsTable } from "./PresetsTable";
+import { PresetsTable } from "./PresetsTable";
 
 
 export const PresetConfigs = () => {
   return (
     <Box sx={{ flex: "1 1 auto" }}>
-      <ConfigsTable />
+      <PresetsTable />
     </Box>
   );
 };
