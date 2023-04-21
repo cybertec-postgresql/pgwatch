d20ec03d730745301395b4559ef849afd53352eb
Denys2002Holub <paloma20022001@gmail.com>
2023-04-21 12:29:22 +0200

    created new file where all 'useQuery & useMutation' hooks will be kept for presets

---
+++ src/webui/src/queries/Preset/index.ts
@@ -0,0 +1,30 @@
+import { AlertColor } from "@mui/material";
+
+import { useMutation } from "@tanstack/react-query";
+import { queryClient } from "queryClient";
+
+import { UseFormReset } from "react-hook-form";
+
+import { QueryKeys } from "queries/queryKeys";
+import { CreatePresetConfigForm } from "queries/types/PresetTypes";
+
+import PresetService from "services/Preset";
+
+const services = PresetService.getInstance();
+
+export const useAddPreset = (
+  handleAlertOpen: (text: string, type: AlertColor) => void,
+  handleClose: () => void,
+  reset: UseFormReset<CreatePresetConfigForm>
+) => useMutation({
+  mutationFn: async (data: CreatePresetConfigForm) => await services.addPreset(data),
+  onSuccess: (_data, variables) => {
+    handleClose();
+    queryClient.invalidateQueries({ queryKey: QueryKeys.preset });
+    handleAlertOpen(`New preset config "${variables.pc_name}" has been successfully added!`, "success");
+    reset();
+  },
+  onError: (error: any) => {
+    handleAlertOpen(error.response.data, "error");
+  }
+});
