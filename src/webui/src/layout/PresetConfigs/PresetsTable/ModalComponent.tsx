c0def3e5a4fd78e5038daa5c6d75ccf870160065
Denys2002Holub <paloma20022001@gmail.com>
2023-04-21 12:32:47 +0200

    created new custom modal component for adding/editing preset configs

--- src/webui/src/layout/PresetConfigs/PresetsTable/ModalComponent.tsx
+++ src/webui/src/layout/PresetConfigs/PresetsTable/ModalComponent.tsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
+
 import CloseIcon from "@mui/icons-material/Close";
 import DoneIcon from "@mui/icons-material/Done";
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
+import { AlertColor, Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Stack, TextField, ToggleButton, ToggleButtonGroup } from "@mui/material";
 
-import { Preset } from "queries/types/PresetTypes";
+import { Controller, FormProvider, SubmitHandler, useForm, useFormContext } from "react-hook-form";
+
+import { useAddPreset } from "queries/Preset";
+import { CreatePresetConfigForm, Preset } from "queries/types/PresetTypes";
+
+import { AddMetric } from "./AddMetric";
 
 
 type Props = {
-  open: boolean,
-  handleClose: () => void,
+  open: boolean;
+  handleClose: () => void;
   recordData: Preset | undefined;
+  handleAlertOpen: (text: string, type: AlertColor) => void;
 };
 
-export const ModalComponent = ({ open, handleClose, recordData }: Props) => {
+export const ModalComponent = ({ open, handleClose, recordData, handleAlertOpen }: Props) => {
+  const methods = useForm<CreatePresetConfigForm>({
+    defaultValues: {
+      pc_config: [{ metric: "", update_interval: 10 }]
+    }
+  });
+  const { handleSubmit, reset } = methods;
+  const addPreset = useAddPreset(handleAlertOpen, handleClose, reset);
+
+  const onSubmit: SubmitHandler<CreatePresetConfigForm> = (result) => {
+    addPreset.mutate(result);
+  };
 
   return (
     <Dialog
@@ -20,16 +39,152 @@ export const ModalComponent = ({ open, handleClose, recordData }: Props) => {
       fullWidth
       maxWidth="md"
     >
-      <DialogTitle>{recordData ? "Edit monitored database" : "Add new database to monitoring"}</DialogTitle>
-      <form>
-        <DialogContent>
-          Dialog Content
-        </DialogContent>
-        <DialogActions>
-          <Button fullWidth onClick={handleClose} size="medium" variant="outlined" startIcon={<CloseIcon />}>Cancel</Button>
-          <Button fullWidth onClick={handleClose} size="medium" variant="contained" startIcon={<DoneIcon />}>{recordData ? "Submit changes" : "Start monitoring"}</Button>
-        </DialogActions>
-      </form>
+      <DialogTitle>{recordData ? "Edit preset config" : "Add new preset config"}</DialogTitle>
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <DialogContent>
+            <ModalContent />
+          </DialogContent>
+          <DialogActions>
+            <Button fullWidth onClick={handleClose} size="medium" variant="outlined" startIcon={<CloseIcon />}>Cancel</Button>
+            <Button type="submit" fullWidth size="medium" variant="contained" startIcon={<DoneIcon />}>{recordData ? "Submit changes" : "Create preset"}</Button>
+          </DialogActions>
+        </form>
+      </FormProvider>
     </Dialog>
+  );
+};
+
+enum Steps {
+  main = "Main",
+  metrics = "Metrics"
+};
+
+type StepType = keyof typeof Steps;
+const defaultStep = Object.keys(Steps)[0] as StepType;
+
+const formErrors = {
+  main: ["pc_name", "pc_description"],
+  metrics: ["pc_config"]
+};
+
+const getStepError = (step: StepType, errors: string[]): boolean => {
+  const fields: string[] = formErrors[step];
+  return errors.some(error => fields.includes(error));
+};
+
+const ModalContent = () => {
+  const { control, formState: { errors } } = useFormContext<CreatePresetConfigForm>();
+  const [activeStep, setActiveStep] = useState<StepType>(defaultStep);
+
+  const handleValidate = (val: string | number) => !!val.toString().trim();
+
+  const stepContent = {
+    main: (
+      <Stack spacing={2}>
+        <Controller
+          name="pc_name"
+          control={control}
+          rules={{
+            required: {
+              value: true,
+              message: "Name is required"
+            },
+            validate: handleValidate
+          }}
+          defaultValue=""
+          render={({ field, fieldState: { error } }) => (
+            <TextField
+              {...field}
+              error={!!error}
+              helperText={error?.message}
+              type="text"
+              label="Name"
+              fullWidth
+            />
+          )}
+        />
+        <Controller
+          name="pc_description"
+          control={control}
+          rules={{
+            required: {
+              value: true,
+              message: "Description is required"
+            },
+            validate: handleValidate
+          }}
+          defaultValue=""
+          render={({ field, fieldState: { error } }) => (
+            <TextField
+              {...field}
+              error={!!error}
+              helperText={error?.message}
+              type="text"
+              label="Description"
+              multiline
+              minRows={2}
+              maxRows={4}
+              fullWidth
+            />
+          )}
+        />
+      </Stack>
+    ),
+    metrics: (
+      <AddMetric control={control} handleValidate={handleValidate} />
+    )
+  };
+
+  const handleChange = (_ev: any, value?: StepType) => {
+    if (value) {
+      setActiveStep(value);
+    }
+  };
+
+  const buttons = Object.entries(Steps).map(([val, label]) => (
+    <ToggleButton
+      fullWidth
+      key={val}
+      value={val}
+      {...(getStepError(val as StepType, Object.keys(errors ?? {})) && {
+        color: "error",
+        selected: true
+      })}
+    >
+      {label}
+    </ToggleButton>
+  ));
+
+  const content = Object.keys(Steps).map((key) => (
+    <Box
+      key={`Content-${key}`}
+      {...(key !== activeStep && {
+        height: 0,
+        overflow: "hidden"
+      })}
+    >
+      <>{stepContent[key as StepType]}</>
+    </Box>
+  ));
+
+  return (
+    <>
+      <ToggleButtonGroup
+        color="primary"
+        value={activeStep}
+        exclusive
+        onChange={handleChange}
+        aria-label="Platform"
+        fullWidth
+      >
+        {buttons}
+      </ToggleButtonGroup>
+      <Box
+        pt={2}
+      >
+        <>{content}</>
+      </Box>
+    </>
   );
 };
