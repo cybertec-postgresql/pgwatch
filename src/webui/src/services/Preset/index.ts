8627b89ee873de736a986c84d635bd727584861f
Denys2002Holub <paloma20022001@gmail.com>
2023-04-21 12:22:22 +0200

    created query for adding new preset config

--- src/webui/src/services/Preset/index.ts
+++ src/webui/src/services/Preset/index.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { CreatePresetConfigForm } from "queries/types/PresetTypes";
 
 
 export default class PresetService {
@@ -23,6 +24,14 @@ export default class PresetService {
   public async deletePreset(pc_name: string) {
     return await axios.delete("/preset", { params: { "id": pc_name } }).
       then(response => response.data).
+      catch(error => {
+        throw error;
+      });
+  };
+
+  public async addPreset(data: CreatePresetConfigForm) {
+    return await axios.post("/preset", data).
+      then(response => response).
       catch(error => {
         throw error;
       });
