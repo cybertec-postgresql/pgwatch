709e53047353227ab39a86a86887590367695c4e
Pavlo Golub <pavlo.golub@gmail.com>
2023-04-25 08:59:12 +0200

    [+] add TimescaleDB metric storage based docker image, resolves #176

---
+++ build-docker-timescale.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker build \
+ --build-arg GIT_TIME=`git show -s --format=%cI HEAD` \
+ --build-arg GIT_HASH=`git show -s --format=%H HEAD` \
+ -t cybertec/pgwatch3-timescale:latest \
+ -f docker/Dockerfile-timescale .
