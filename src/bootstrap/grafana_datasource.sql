fc0bcf5e6e5a4d157b23e60b90dbf6349a168db2
Pavlo Golub <pavlo.golub@gmail.com>
2023-04-26 17:53:59 +0200

    [!] switch to Grafana REST API for dashboard provisioning, closes #188

--- src/bootstrap/grafana_datasource.sql
+++
@@ -1,6 +0,0 @@
-insert into data_source (org_id, version, type, name, access, url,
-  password, "user", database, basic_auth, is_default, json_data, created, updated
-  ) values (
-  1, 0, 'influxdb', 'Influx', 'proxy', 'http://localhost:8086',
-  'root', 'root', 'pgwatch3', 'f', 't', '{}', now(), now()
-  );
--- src/bootstrap/supervisord_bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for prog in postgres grafana grafana_dashboard_setup influxdb pgwatch3 ; do
-  echo "supervisorctl start $prog ..."
-  supervisorctl start $prog
-  echo "sleep 5"
-  sleep 5
-done
