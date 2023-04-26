c58a138816383bc205d6702e447efd4e317b2eab
Pavlo Golub <pavlo.golub@gmail.com>
2023-04-26 18:06:21 +0200

    remove obsolete sql

--- src/bootstrap/grafana_datasource_pg.sql
+++
@@ -1,6 +0,0 @@
-insert into data_source (org_id, version, type, name, access, url,
-  password, "user", database, basic_auth, is_default, json_data, created, updated
-  ) values (
-  1, 0, 'postgres', 'pg-metrics', 'proxy', 'localhost:5432',
-  'pgwatch3admin', 'pgwatch3', 'pgwatch3_metrics', 'f', 't', '{"postgresVersion":1400,"sslmode":"disable","timescaledb":false}', now(), now()
-  );
