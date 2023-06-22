05c6baf7bb72da18bfbf1608c16ee260ba7274c5
Pavlo Golub <pavlo.golub@gmail.com>
2023-06-22 18:23:17 +0200

    use pgwatch3 user for testdb init

--- src/bootstrap/init_supervisor.sh
+++ src/bootstrap/init_supervisor.sh
@@ -10,7 +10,7 @@ for prog in postgres grafana grafana_dashboard_setup pgwatch3 ; do
 done
 
 if [ -n "$PW3_TESTDB" ] ; then
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "pgwatch3" <<-EOSQL
+    psql -v ON_ERROR_STOP=1 --username "pgwatch3" --dbname "pgwatch3" <<-EOSQL
         INSERT INTO pgwatch3.monitored_db (md_unique_name, md_preset_config_name, md_config, md_hostname, md_port, md_dbname, md_user, md_password)
         SELECT 'test', 'exhaustive', null, 'localhost', '5432', 'pgwatch3', 'pgwatch3', 'pgwatch3admin'
         WHERE NOT EXISTS (
