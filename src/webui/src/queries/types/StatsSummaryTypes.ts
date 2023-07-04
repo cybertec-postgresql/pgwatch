1ea8ce8ccb6a81a7cd2efa2463f9bbe33c23599c
Denys2002Holub <paloma20022001@gmail.com>
2023-07-04 14:17:16 +0200

    null type added

--- src/webui/src/queries/types/StatsSummaryTypes.ts
+++ src/webui/src/queries/types/StatsSummaryTypes.ts
@@ -3,12 +3,12 @@ export type StatsSummary = {
     [key: string]: string | number;
   },
   metrics: {
-    [key: string]: string | number;
+    [key: string]: string | number | null;
   },
   datastore: {
-    [key: string]: string | number;
+    [key: string]: string | number | null;
   },
   general: {
-    [key: string]: string | number;
+    [key: string]: string | number | null;
   }
 };
