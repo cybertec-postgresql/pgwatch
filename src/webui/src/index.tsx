b6f80244a348bad0e1c0a7d2e03fa1f3856c1fa4
Denys2002Holub <paloma20022001@gmail.com>
2023-07-19 14:15:55 +0200

    compiled warnings fixed

--- src/webui/src/index.tsx
+++ src/webui/src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { AlertProvider } from "utils/AlertContext";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { AlertProvider } from "utils/AlertContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
