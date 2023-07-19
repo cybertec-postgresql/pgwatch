2500a114ddc0050ae36b2383bd8ce62fe8760b44
Denys2002Holub <paloma20022001@gmail.com>
2023-07-19 14:13:03 +0200

    compiled warnings fixed

--- src/webui/src/utils/AlertContext.tsx
+++ src/webui/src/utils/AlertContext.tsx
@@ -1,11 +1,11 @@
+import { createContext, useContext, useState } from "react";
 import { AlertColor } from "@mui/material";
-import { useState, createContext, useContext, useEffect } from "react";
 
 export type AlertContextType = {
   open: boolean;
   severity: AlertColor;
   message: string;
-  callAlert: (severity: AlertColor, message: string) => void;
+  callAlert: (alertSeverity: AlertColor, alertMessage: string) => void;
   closeAlert: () => void;
 };
 
@@ -20,15 +20,15 @@ export const AlertProvider = ({ children }: AlertProviderProps) => {
   const [severity, setSeverity] = useState<AlertColor>("success");
   const [message, setMessage] = useState("");
 
-  const callAlert = (severity: AlertColor, message: string) => {
-    setSeverity(severity);
-    setMessage(message);
+  const callAlert = (alertSeverity: AlertColor, alertMessage: string) => {
+    setSeverity(alertSeverity);
+    setMessage(alertMessage);
     setOpen(true);
   };
 
   const closeAlert = () => {
     setOpen(false);
-  }
+  };
 
   return (
     <AlertContext.Provider value={{ open, severity, message, callAlert, closeAlert }}>
