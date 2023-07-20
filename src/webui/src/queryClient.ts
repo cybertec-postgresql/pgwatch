756b48c1128d6d4a71d6f53dba701091d7f495ff
Denys2002Holub <paloma20022001@gmail.com>
2023-07-20 14:28:42 +0200

    method name changed

--- src/webui/src/queryClient.ts
+++ src/webui/src/queryClient.ts
@@ -1,13 +1,13 @@
 import { MutationCache, QueryCache, QueryClient } from "@tanstack/react-query";
 import axios from "axios";
-import { isUnathorized } from "axiosInstance";
+import { isUnauthorized } from "axiosInstance";
 import { removeToken } from "services/Token";
 
 export const queryClient = new QueryClient({
     queryCache: new QueryCache({
         onError: (error: any) => {
             if (axios.isAxiosError(error)) {
-                if (isUnathorized(error)) {
+                if (isUnauthorized(error)) {
                     removeToken();
                 }
             }
@@ -16,7 +16,7 @@ export const queryClient = new QueryClient({
     mutationCache: new MutationCache({
         onError: (error: any) => {
             if (axios.isAxiosError(error)) {
-                if (isUnathorized(error)) {
+                if (isUnauthorized(error)) {
                     removeToken();
                 }
             }
