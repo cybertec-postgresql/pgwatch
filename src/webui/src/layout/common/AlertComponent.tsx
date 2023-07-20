f87ee671dae675132ad51736ae4d524c45f0c402
Denys2002Holub <paloma20022001@gmail.com>
2023-07-20 13:27:19 +0200

    styles added

--- src/webui/src/layout/common/AlertComponent.tsx
+++ src/webui/src/layout/common/AlertComponent.tsx
@@ -1,12 +1,18 @@
-import { Alert, Snackbar } from "@mui/material";
+import { Alert, Snackbar, Tooltip, Typography } from "@mui/material";
 import { useAlert } from "utils/AlertContext";
 
 export const AlertComponent = () => {
   const { open, severity, message, closeAlert } = useAlert();
 
   return (
-    <Snackbar open={open} autoHideDuration={3000} onClose={closeAlert}>
-      <Alert sx={{ minWidth: 400 }} variant="filled" severity={severity}>{message}</Alert>
+    <Snackbar open={open} onClose={closeAlert}>
+      <Alert sx={{ minWidth: 400, maxWidth: 400, whiteSpace: "nowrap", alignItems: "center" }} variant="filled" severity={severity}>
+        <Tooltip title={message}>
+          <Typography sx={{ overflowX: "hidden" }}>
+            {message}
+          </Typography>
+        </Tooltip>
+      </Alert>
     </Snackbar>
   );
 };
