9a3016f5c8f13328ca41d4020fcec93606f29141
Denys2002Holub <paloma20022001@gmail.com>
2023-07-21 12:52:29 +0200

    mutation directly implemented

--- src/webui/src/layout/common/DbEnableSwitchComponent.tsx
+++ src/webui/src/layout/common/DbEnableSwitchComponent.tsx
@@ -1,21 +1,24 @@
 import { useState } from "react";
 
 import { Switch } from "@mui/material";
-import { UseMutationResult } from "@tanstack/react-query";
-import { AxiosResponse } from "axios";
-import { updateEnabledDbForm } from "queries/types/DbTypes";
+import { useNavigate } from "react-router-dom";
+import { useEditEnableDb } from "queries/Dashboard";
+import { useAlert } from "utils/AlertContext";
 
 
 type Props = {
   id: string,
-  value: boolean,
-  editEnable: UseMutationResult<AxiosResponse<any, any>, any, updateEnabledDbForm, unknown>
+  value: boolean
 };
 
-export const DbEnableSwitchComponent = ({ id, value, editEnable }: Props) => {
+export const DbEnableSwitchComponent = ({ id, value }: Props) => {
   const [checked, setChecked] = useState(value);
+  const { callAlert } = useAlert();
+  const navigate = useNavigate();
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>, changedValue: boolean) => {
+  const editEnable = useEditEnableDb(callAlert, navigate);
+
+  const handleChange = (_event: React.ChangeEvent<HTMLInputElement>, changedValue: boolean) => {
     setChecked(changedValue);
     editEnable.mutate({ md_unique_name: id, data: { md_is_enabled: changedValue } });
   };
