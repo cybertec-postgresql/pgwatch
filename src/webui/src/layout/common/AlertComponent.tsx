fbcc22cd9eb128dd3e7bed4d7f609fcc8628380d
Denys2002Holub <paloma20022001@gmail.com>
2023-07-21 12:39:53 +0200

    auto hide duration set

--- src/webui/src/layout/common/AlertComponent.tsx
+++ src/webui/src/layout/common/AlertComponent.tsx
@@ -5,7 +5,7 @@ export const AlertComponent = () => {
   const { open, severity, message, closeAlert } = useAlert();
 
   return (
-    <Snackbar open={open} onClose={closeAlert}>
+    <Snackbar open={open} autoHideDuration={4000} onClose={closeAlert}>
       <Alert sx={{ minWidth: 400, maxWidth: 400, whiteSpace: "nowrap", alignItems: "center" }} variant="filled" severity={severity}>
         <Tooltip title={message}>
           <Typography sx={{ overflowX: "hidden" }}>
