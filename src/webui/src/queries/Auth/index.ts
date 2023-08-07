24c9045702fbe27e727300500f96989c29bad5fe
Denys2002Holub <paloma20022001@gmail.com>
2023-08-07 12:31:29 +0200

    new request for default login implemented

--- src/webui/src/queries/Auth/index.ts
+++ src/webui/src/queries/Auth/index.ts
@@ -1,4 +1,5 @@
-import { useMutation } from "@tanstack/react-query";
+import { AlertColor } from "@mui/material";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { NavigateFunction } from "react-router-dom";
 import { AuthForm } from "queries/types/AuthTypes";
 import AuthService from "services/Auth";
@@ -7,10 +8,21 @@ import { removeToken, setToken } from "services/Token";
 const services = AuthService.getInstance();
 
 export const useLogin = (navigate: NavigateFunction) => useMutation({
-  mutationFn: async(data: AuthForm) => await services.login(data),
+  mutationFn: async (data: AuthForm) => await services.login(data),
   onSuccess: (data) => {
     setToken(data);
     navigate("/dashboard");
+  }
+});
+
+export const useLoginDefault = (navigate: NavigateFunction, callAlert: (alertSeverity: AlertColor, alertMessage: string) => void) => useQuery({
+  queryFn: async () => {
+    await services.loginDefault().
+      then(data => {
+        setToken(data);
+        navigate("/dashboard");
+      }).
+      catch(() => callAlert("info", "Provide your credentials"));
   }
 });
 
