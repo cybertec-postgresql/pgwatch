58554890b1b29e524b28003db573177958255849
Denys2002Holub <paloma20022001@gmail.com>
2023-08-08 11:42:09 +0200

    option replace to navigate action added

--- src/webui/src/queries/Auth/index.ts
+++ src/webui/src/queries/Auth/index.ts
@@ -1,5 +1,4 @@
-import { AlertColor } from "@mui/material";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { NavigateFunction } from "react-router-dom";
 import { AuthForm } from "queries/types/AuthTypes";
 import AuthService from "services/Auth";
@@ -11,22 +10,11 @@ export const useLogin = (navigate: NavigateFunction) => useMutation({
   mutationFn: async (data: AuthForm) => await services.login(data),
   onSuccess: (data) => {
     setToken(data);
-    navigate("/dashboard");
-  }
-});
-
-export const useLoginDefault = (navigate: NavigateFunction, callAlert: (alertSeverity: AlertColor, alertMessage: string) => void) => useQuery({
-  queryFn: async () => {
-    await services.loginDefault().
-      then(data => {
-        setToken(data);
-        navigate("/dashboard");
-      }).
-      catch(() => callAlert("info", "Provide your credentials"));
+    navigate("/dashboard", { replace: true });
   }
 });
 
 export const logout = (navigate: NavigateFunction) => {
   removeToken();
-  navigate("/");
+  navigate("/", { replace: true });
 };
