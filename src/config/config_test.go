b56e388a022f5404038a05e262b1c331ae8d9dce
Pavlo Golub <pavlo.golub@gmail.com>
2023-09-28 15:10:06 +0200

    add tests

---
+++ src/config/config_test.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfig(t *testing.T) {
+	os.Args = []string{0: "config_test", "--config=instances.yaml"}
+	_, err := NewConfig(nil)
+	assert.NoError(t, err)
+
+	os.Args = []string{0: "config_test", "--unknown"}
+	_, err = NewConfig(nil)
+	assert.Error(t, err)
+
+	os.Args = []string{0: "config_test"} // clientname arg is missing
+	_, err = NewConfig(nil)
+	assert.Error(t, err)
+
+	os.Args = []string{0: "config_test", "--config=foo.boo.bar.baz.yaml"}
+	_, err = NewConfig(nil)
+	assert.Error(t, err)
+
+	os.Args = []string{0: "config_test"} // clientname arg is missing, but set PGTT_CLIENTNAME
+	assert.NoError(t, os.Setenv("PGTT_CLIENTNAME", "worker001"))
+	_, err = NewConfig(nil)
+	assert.NoError(t, err)
+}
