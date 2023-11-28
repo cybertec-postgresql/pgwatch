2648161f2dfe9bd67b8de1bae8415095bfca92ab
Denys2002Holub <paloma20022001@gmail.com>
2023-11-28 13:41:21 +0100

    query param added

--- src/webui/src/queries/Log/index.ts
+++ src/webui/src/queries/Log/index.ts
@@ -1,6 +1,11 @@
 import { useWebSocket } from "react-use-websocket/dist/lib/use-websocket";
-
+import { getToken } from "services/Token";
 
 const socketUrl = "ws://localhost:8080/log";
+const token = getToken();
 
-export const useLogs = () => useWebSocket(socketUrl);
+export const useLogs = () => useWebSocket(socketUrl, {
+  queryParams: {
+    "Token": token ? token : ""
+  }
+});
