006227c50434ecf5fcde0c9575f60b3ec66981cb
Pavlo Golub <pavlo.golub@gmail.com>
2024-02-16 16:12:28 +0100

    [+] bump Node to v21, Postgres to v16, Grafana to v10.3 in docker images * bump versions * use new ENVs, e.g. `PW3_SINK` * fix docker bootstrap script

--- docker/bootstrap/init_test_monitored_db.sh
+++ docker/bootstrap/init_test_monitored_db.sh
@@ -2,10 +2,8 @@
 
 if [ -n "$PW3_TESTDB" ] ; then
   psql -v ON_ERROR_STOP=1 --username "pgwatch3" --dbname "pgwatch3" <<-EOSQL
-      INSERT INTO pgwatch3.monitored_db (md_unique_name, md_preset_config_name, md_config, md_hostname, md_port, md_dbname, md_user, md_password)
-      SELECT 'test', 'exhaustive', null, 'localhost', '5432', 'pgwatch3', 'pgwatch3', 'pgwatch3admin'
-      WHERE NOT EXISTS (
-          SELECT * FROM pgwatch3.monitored_db WHERE (md_unique_name, md_hostname, md_dbname) = ('test', 'localhost', 'pgwatch3')
-      );
+      INSERT INTO pgwatch3.monitored_db (md_name, md_preset_config_name, md_config, md_connstr)
+      SELECT 'test', 'exhaustive', null, 'postgresql://pgwatch3:pgwatch3admin@localhost:5432/pgwatch3'
+      WHERE NOT EXISTS (SELECT * FROM pgwatch3.monitored_db WHERE md_name = 'test');
 EOSQL
 fi
