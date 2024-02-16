231f1faee1319d98c65a0346172ecc821cb442e4
Pavlo Golub <pavlo.golub@gmail.com>
2024-02-16 15:36:55 +0100

    [-] allow 0.0.0.0:8080 connections by default to WebUI admin

--- src/config/cmdoptions.go
+++ src/config/cmdoptions.go
@@ -72,7 +72,7 @@ type LoggingOpts struct {
 
 // WebUIOpts specifies the internal web UI server options
 type WebUIOpts struct {
-	WebAddr     string `long:"web-addr" mapstructure:"web-addr" description:"TCP address in the form 'host:port' to listen on" default:"127.0.0.1:8080" env:"PW3_WEBADDR"`
+	WebAddr     string `long:"web-addr" mapstructure:"web-addr" description:"TCP address in the form 'host:port' to listen on" default:":8080" env:"PW3_WEBADDR"`
 	WebUser     string `long:"web-user" mapstructure:"web-user" description:"Admin login" env:"PW3_WEBUSER"`
 	WebPassword string `long:"web-password" mapstructure:"web-password" description:"Admin password" env:"PW3_WEBPASSWORD"`
 }
