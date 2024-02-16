7e2aa9a32631b0f8e00262c9de22dd028e5c3512
Pavlo Golub <pavlo.golub@gmail.com>
2024-02-16 09:32:45 +0100

    [*] prettify `config`, remove unused opts (#367)

--- src/config/cmdoptions_test.go
+++ src/config/cmdoptions_test.go
@@ -4,8 +4,16 @@ import (
 	"os"
 	"testing"
 
+	flags "github.com/jessevdk/go-flags"
 	"github.com/stretchr/testify/assert"
 )
+
+// NewCmdOptions returns a new instance of CmdOptions with default values
+func NewCmdOptions(args ...string) *Options {
+	cmdOpts := new(Options)
+	_, _ = flags.NewParser(cmdOpts, flags.PrintErrors).ParseArgs(args)
+	return cmdOpts
+}
 
 func TestParseFail(t *testing.T) {
 	tests := [][]string{
@@ -14,7 +22,7 @@ func TestParseFail(t *testing.T) {
 	}
 	for _, d := range tests {
 		os.Args = d
-		_, err := NewConfig(nil)
+		_, err := New(nil)
 		assert.Error(t, err)
 	}
 }
@@ -26,7 +34,7 @@ func TestParseSuccess(t *testing.T) {
 	}
 	for _, d := range tests {
 		os.Args = d
-		_, err := NewConfig(nil)
+		_, err := New(nil)
 		assert.NoError(t, err)
 	}
 }
@@ -41,4 +49,19 @@ func TestLogLevel(t *testing.T) {
 func TestNewCmdOptions(t *testing.T) {
 	c := NewCmdOptions("-c", "config_unit_test", "--password=somestrong")
 	assert.NotNil(t, c)
+}
+
+func TestConfig(t *testing.T) {
+	os.Args = []string{0: "config_test", "--config=sample.config.yaml"}
+	_, err := New(nil)
+	assert.NoError(t, err)
+
+	os.Args = []string{0: "config_test", "--unknown"}
+	_, err = New(nil)
+	assert.Error(t, err)
+
+	os.Args = []string{0: "config_test"} // clientname arg is missing, but set PW3_CONFIG
+	assert.NoError(t, os.Setenv("PW3_CONFIG", "postgresql://foo:baz@bar/test"))
+	_, err = New(nil)
+	assert.NoError(t, err)
 }
