e89bfcc11f8873d145334d36cd056b11f8a20760
Pavlo Golub <pavlo.golub@gmail.com>
2024-03-04 15:52:55 +0100

    [-] remove references to deprecated helpers

--- docker/bootstrap/2_init_pgwatch_db.sh
+++ docker/bootstrap/2_init_pgwatch_db.sh
@@ -7,12 +7,8 @@ EOSQL
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "pgwatch3" \
     -f /pgwatch3/metrics/00_helpers/get_load_average/9.1/metric.sql \
-    -f /pgwatch3/metrics/00_helpers/get_stat_statements/9.4/metric.sql \
-    -f /pgwatch3/metrics/00_helpers/get_stat_activity/9.2/metric.sql \
-    -f /pgwatch3/metrics/00_helpers/get_stat_replication/9.2/metric.sql \
     -f /pgwatch3/metrics/00_helpers/get_table_bloat_approx/9.5/metric.sql \
     -f /pgwatch3/metrics/00_helpers/get_table_bloat_approx_sql/12/metric.sql \
-    -f /pgwatch3/metrics/00_helpers/get_wal_size/10/metric.sql \
     -f /pgwatch3/metrics/00_helpers/get_psutil_cpu/9.1/metric.sql \
     -f /pgwatch3/metrics/00_helpers/get_psutil_mem/9.1/metric.sql \
     -f /pgwatch3/metrics/00_helpers/get_psutil_disk/9.1/metric.sql \
