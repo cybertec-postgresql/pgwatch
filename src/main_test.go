cc5c55475aafb02d47411fb18e7911ef15c46c4c
Pavlo Golub <pavlo.golub@gmail.com>
2024-03-20 17:16:53 +0100

    [!] apply all changes to main.go

--- src/main_test.go
+++ src/main_test.go
@@ -5,14 +5,14 @@ import "testing"
 func TestVersionToInt(t *testing.T) {
 	tests := []struct {
 		arg  string
-		want uint
+		want int
 	}{
 		{"", 0},
 		{"foo", 0},
-		{"13", 1300},
-		{"3.0", 300},
-		{"9.6.3", 906},
-		{"v9.6-beta2", 906},
+		{"13", 13_00_00},
+		{"3.0", 3_00_00},
+		{"9.6.3", 9_06_03},
+		{"v9.6-beta2", 9_06_00},
 	}
 	for _, tt := range tests {
 		if got := VersionToInt(tt.arg); got != tt.want {
