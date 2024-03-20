9eeddf0e3d84c33b84d33756f9324e8878ab509d
Pavlo Golub <pavlo.golub@gmail.com>
2024-03-20 17:01:02 +0100

    remove helpers and fix table name in docker related files

--- docker/bootstrap/init_test_monitored_db.sh
+++ docker/bootstrap/init_test_monitored_db.sh
@@ -2,8 +2,8 @@
 
 if [ -n "$PW3_TESTDB" ] ; then
   psql -v ON_ERROR_STOP=1 --username "pgwatch3" --dbname "pgwatch3" <<-EOSQL
-      INSERT INTO pgwatch3.monitored_db (md_name, md_preset_config_name, md_config, md_connstr)
+      INSERT INTO pgwatch3.source (name, preset_config_name, config, connstr)
       SELECT 'test', 'exhaustive', null, 'postgresql://pgwatch3:pgwatch3admin@localhost:5432/pgwatch3'
-      WHERE NOT EXISTS (SELECT * FROM pgwatch3.monitored_db WHERE md_name = 'test');
+      WHERE NOT EXISTS (SELECT * FROM pgwatch3.monitored_db WHERE name = 'test');
 EOSQL
 fi
