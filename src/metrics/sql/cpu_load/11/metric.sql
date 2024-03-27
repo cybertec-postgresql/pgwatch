58be7adc5056b6093d78fd8f5774b916fade8926
Pavlo Golub <pavlo.golub@gmail.com>
2024-03-27 10:57:41 +0100

    removed old sql metric definitions

--- src/metrics/sql/cpu_load/11/metric.sql
+++
@@ -1,7 +0,0 @@
-select /* pgwatch3_generated */
-  (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
-  round(load_1min::numeric, 2)::float as load_1min,
-  round(load_5min::numeric, 2)::float as load_5min,
-  round(load_15min::numeric, 2)::float as load_15min
-from
-  get_load_average();   -- needs the plpythonu proc from "metric_fetching_helpers" folder
