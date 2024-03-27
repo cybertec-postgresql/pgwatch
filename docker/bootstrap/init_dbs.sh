ee9efbdc9f8e085f565c03a3f5be66f61998ecff
Pavlo Golub <pavlo.golub@gmail.com>
2024-03-27 18:53:40 +0100

    [*] improve add-test-db in docker compose file

--- docker/bootstrap/init_dbs.sh
+++ docker/bootstrap/init_dbs.sh
@@ -26,11 +26,3 @@ if [ "$PW3_PG_SCHEMA_TYPE" == "timescale" ] ; then
         CREATE EXTENSION timescaledb;
 EOSQL
 fi
-
-if [ -n "$PW3_TESTDB" ] ; then
-  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "pgwatch3" <<-EOSQL
-      INSERT INTO pgwatch3.source (name, preset_config, config, connstr)
-      SELECT 'test', 'exhaustive', null, 'postgresql://pgwatch3:pgwatch3admin@localhost:5432/pgwatch3'
-      WHERE NOT EXISTS (SELECT * FROM pgwatch3.source WHERE name = 'test');
-EOSQL
-fi
