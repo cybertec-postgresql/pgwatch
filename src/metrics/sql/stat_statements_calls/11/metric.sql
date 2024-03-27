af656c3cb25a6b0b560cfc55b69013ba4a429511
Pavlo Golub <pavlo.golub@gmail.com>
2024-03-27 10:57:41 +0100

    removed old sql metric definitions

--- src/metrics/sql/stat_statements_calls/11/metric.sql
+++
@@ -1,9 +0,0 @@
-select /* pgwatch3_generated */
-  (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
-  coalesce(sum(calls), 0)::int8 as calls,
-  coalesce(round(sum(total_time)::numeric, 3), 0)::float8 as total_time
-from
-  pg_stat_statements
-where
-  dbid = (select oid from pg_database where datname = current_database())
-;
