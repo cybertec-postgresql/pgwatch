095918d902be06f8fe633de6084945d472a3608b
denys-holub <paloma20022001@gmail.com>
2024-04-05 16:46:04 +0200

    [*] updated due to new changes

--- src/webui/src/queries/Metric/index.ts
+++ src/webui/src/queries/Metric/index.ts
@@ -1,42 +1,27 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { UseFormReset } from "react-hook-form";
-import { QueryKeys } from "queries/queryKeys";
+import { QueryKeys } from "consts/queryKeys";
+import { Metrics } from "types/Metric/Metric";
+import { MetricRequestBody } from "types/Metric/MetricRequestBody";
 import MetricService from "services/Metric";
-import { createMetricForm, updateMetricForm } from "../types/MetricTypes";
-import { Metrics } from "layout/MetricDefinitions/MetricDefinitions.types";
 
 const services = MetricService.getInstance();
 
 export const useMetrics = () => useQuery<Metrics>({
-  queryKey: QueryKeys.metric,
+  queryKey: [QueryKeys.Metric],
   queryFn: async () => await services.getMetrics()
 });
 
 export const useDeleteMetric = () => useMutation({
-  mutationKey: QueryKeys.metric,
-  mutationFn: async (data: number) => await services.deleteMetric(data)
+  mutationKey: [QueryKeys.Metric],
+  mutationFn: async (data: string) => await services.deleteMetric(data)
 });
 
-export const useEditMetric = (
-  handleClose: () => void,
-  reset: UseFormReset<createMetricForm>
-) => useMutation({
-  mutationKey: QueryKeys.metric,
-  mutationFn: async (data: updateMetricForm) => await services.editMetric(data),
-  onSuccess: () => {
-    handleClose();
-    reset();
-  }
+export const useEditMetric = () => useMutation({
+  mutationKey: [QueryKeys.Metric],
+  mutationFn: async (data: MetricRequestBody) => await services.editMetric(data),
 });
 
-export const useAddMetric = (
-  handleClose: () => void,
-  reset: UseFormReset<createMetricForm>
-) => useMutation({
-  mutationKey: QueryKeys.metric,
-  mutationFn: async (data: createMetricForm) => await services.addMetric(data),
-  onSuccess: () => {
-    handleClose();
-    reset();
-  }
+export const useAddMetric = () => useMutation({
+  mutationKey: [QueryKeys.Metric],
+  mutationFn: async (data: MetricRequestBody) => await services.addMetric(data),
 });
--- src/webui/src/services/Metric/index.ts
+++ src/webui/src/services/Metric/index.ts
@@ -1,6 +1,6 @@
 import { axiosInstance } from "axiosInstance";
-import { Metrics } from "layout/MetricDefinitions/MetricDefinitions.types";
-import { Metric, createMetricForm, updateMetricForm } from "queries/types/MetricTypes";
+import { Metrics } from "types/Metric/Metric";
+import { MetricRequestBody } from "types/Metric/MetricRequestBody";
 
 
 export default class MetricService {
@@ -19,18 +19,18 @@ export default class MetricService {
       then(response => response.data);
   };
 
-  public async deleteMetric(m_id: number) {
-    return await axiosInstance.delete("metric", { params: { "id": m_id } }).
+  public async deleteMetric(data: string) {
+    return await axiosInstance.delete("metric", { params: { "key": data } }).
       then(response => response.data);
   };
 
-  public async addMetric(data: createMetricForm) {
-    return await axiosInstance.post("metric", data).
+  public async addMetric(data: MetricRequestBody) {
+    return await axiosInstance.post("metric", data.Data, { params: { "name": data.Name } }).
       then(response => response);
   };
 
-  public async editMetric(data: updateMetricForm) {
-    return await axiosInstance.patch("metric", data.data, { params: { "id": data.m_id } }).
+  public async editMetric(data: MetricRequestBody) {
+    return await axiosInstance.patch("metric", data.Data, { params: { "name": data.Name } }).
       then(response => response);
   };
 }
