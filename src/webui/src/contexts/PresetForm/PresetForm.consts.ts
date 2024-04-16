8bfb37953630439b7b3ababff621c41ba670ec9b
denys-holub <paloma20022001@gmail.com>
2024-04-16 10:42:09 +0200

    [+] create context for preset form

---
+++ src/webui/src/contexts/PresetForm/PresetForm.consts.ts
@@ -0,0 +1,9 @@
+import { PresetFormContextType } from "./PresetForm.types";
+
+export const PresetFormContextDefaults: PresetFormContextType = {
+  data: undefined,
+  setData: () => undefined,
+  open: false,
+  handleOpen: () => undefined,
+  handleClose: () => undefined,
+};
