2628869670b962dc7371f97cac1f984ece1f5474
denys-holub <paloma20022001@gmail.com>
2024-05-06 12:38:07 +0200

    [+] create `Autocomplete` component

---
+++ src/webui/src/components/Autocomplete/Autocomplete.tsx
@@ -0,0 +1,33 @@
+import { AutocompleteRenderInputParams, Autocomplete as MuiAutocomplete, TextField } from "@mui/material";
+import { ControllerRenderProps } from "react-hook-form";
+
+type Props = {
+  id?: string;
+  label: string;
+  options: { label: string }[];
+  loading?: boolean;
+  error?: boolean;
+} & ControllerRenderProps;
+
+export const Autocomplete = (props: Props) => {
+  const { id, label, options, loading, error, ...field } = props;
+
+  const customInput = (params: AutocompleteRenderInputParams) => (
+    <TextField
+      {...params}
+      label={label}
+      error={error}
+    />
+  );
+
+  return (
+    <MuiAutocomplete
+      {...field}
+      id={id}
+      options={options}
+      renderInput={customInput}
+      onChange={(_, value) => field.onChange(value.label ? value.label : "")}
+      loading={loading}
+    />
+  );
+};
