858c14c616b829b4bda0c9941eb14f4f04c73479
Pavlo Golub <pavlo.golub@gmail.com>
2024-07-17 17:23:07 +0200

    [*] move docker scripts to appropriate folder (#477)

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$(dirname "$SCRIPT_DIR")"
 docker build \
  --build-arg GIT_TIME=`git show -s --format=%cI HEAD` \
  --build-arg GIT_HASH=`git show -s --format=%H HEAD` \
  --build-arg VERSION=`git rev-parse --abbrev-ref HEAD` \
- -t cybertec/pgwatch3-demo:latest \
- -f docker/demo/Dockerfile .
+ -t cybertec/pgwatch3:latest \
+ -f docker/Dockerfile .
