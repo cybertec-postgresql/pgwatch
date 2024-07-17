db41eb233fec4d4d5047bc86754326ed85388d66
Pavlo Golub <pavlo.golub@gmail.com>
2024-07-17 16:59:58 +0200

    [*] move docker scripts to appropriate folder

--- docker/build-docker-demo.sh
+++ docker/build-docker-demo.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$(dirname "$SCRIPT_DIR")"
 docker build \
  --build-arg GIT_TIME=`git show -s --format=%cI HEAD` \
  --build-arg GIT_HASH=`git show -s --format=%H HEAD` \
