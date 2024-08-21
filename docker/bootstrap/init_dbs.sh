be7332239ca9612bc5a4033e659c8b4bcc5da270
Pavlo Golub <pavlo.golub@gmail.com>
2024-08-21 17:51:11 +0200

    [*] change Docker image names to `cybertecpostgresql/pgwatch` (#507)
    * [*] change Docker image names to `cybertecpostgresql/pgwatch`
    * [-] fix PW3_CONFIG to PW3_SOURCES
    * [*] hard code postgres user for pgwatch-demo image
    * [-] fix image name in shell build scripts

--- docker/bootstrap/init_dbs.sh
+++ docker/bootstrap/init_dbs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "pgwatch3" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "pgwatch3" <<-EOSQL
     CREATE EXTENSION pg_qualstats;
     CREATE EXTENSION plpython3u;
     CREATE EXTENSION pg_stat_statements;
@@ -8,7 +8,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "pgwatch3" <<-EOSQL
     GRANT EXECUTE ON FUNCTION pg_stat_file(text, boolean) TO pgwatch3;
 EOSQL
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "pgwatch3" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "pgwatch3" <<-EOSQL
 BEGIN;
 CREATE OR REPLACE FUNCTION get_load_average(OUT load_1min float, OUT load_5min float, OUT load_15min float) AS
 '
--- docker/bootstrap/init_test_monitored_db.sh
+++ docker/bootstrap/init_test_monitored_db.sh
@@ -2,7 +2,7 @@
 
 if [ -n "$PW3_TESTDB" ] ; then
   echo "adding test monitored database..."
-  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "pgwatch3" <<-EOSQL
+  psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "pgwatch3" <<-EOSQL
       INSERT INTO pgwatch3.source (name, preset_config, config, connstr)
       SELECT 'test', 'exhaustive', null, 'postgresql://pgwatch3:pgwatch3admin@localhost:5432/pgwatch3'
       WHERE NOT EXISTS (SELECT * FROM pgwatch3.source WHERE name = 'test');
