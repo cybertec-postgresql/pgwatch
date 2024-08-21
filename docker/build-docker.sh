650b40f1dc9bbd69b6bcdea2316321826ed378c5
Pavlo Golub <pavlo.golub@gmail.com>
2024-08-21 17:15:06 +0200

    [-] fix image name in shell build scripts

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -6,5 +6,5 @@ docker build \
  --build-arg GIT_TIME=`git show -s --format=%cI HEAD` \
  --build-arg GIT_HASH=`git show -s --format=%H HEAD` \
  --build-arg VERSION=`git rev-parse --abbrev-ref HEAD` \
- -t cybertec/pgwatch3:latest \
+ -t cybertecpostgresql/pgwatch:latest \
  -f docker/Dockerfile .
