c5efc731391a9a5ef81631bf0a2221d45e6cac90
denys-holub <paloma20022001@gmail.com>
2024-08-27 11:58:08 +0200

    fix(frontend): websocket url

--- internal/webui/src/queries/Log/index.ts
+++ internal/webui/src/queries/Log/index.ts
@@ -1,7 +1,7 @@
 import { useWebSocket } from "react-use-websocket/dist/lib/use-websocket";
 import { getToken } from "services/Token";
 
-const socketUrl = "ws://localhost:8080/log";
+const socketUrl = `ws://${window.location.origin}/log`;
 const token = getToken();
 
 export const useLogs = () => useWebSocket(socketUrl, {
