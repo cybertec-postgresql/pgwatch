d4a32b6d56b8601f313c957ad334571242ca47f2
Pavlo Golub <pavlo.golub@gmail.com>
2024-08-27 16:19:11 +0200

    [+] add `RPCSyncRequest` to `api`

--- api/types.go
+++ api/types.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"github.com/cybertec-postgresql/pgwatch/v3/internal/metrics"
+	"github.com/cybertec-postgresql/pgwatch/v3/internal/sinks"
 )
 
 type (
@@ -10,4 +11,6 @@ type (
 	// MeasurementEnvelope represents a collection of measurement messages wrapped up
 	// with metadata such as metric name, source type, etc.
 	MeasurementEnvelope = metrics.MeasurementEnvelope
+	// RPCSyncRequest represents a request to sync metrics with the remote RPC sink
+	RPCSyncRequest = sinks.SyncReq
 )
