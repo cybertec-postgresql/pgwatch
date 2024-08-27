e647dd97d69c066c8880a9d095800470b9c9f892
denys-holub <paloma20022001@gmail.com>
2024-08-27 12:37:32 +0200

    chore(frontend): define socket protocol

--- internal/webui/src/queries/Log/index.ts
+++ internal/webui/src/queries/Log/index.ts
@@ -1,11 +1,16 @@
 import { useWebSocket } from "react-use-websocket/dist/lib/use-websocket";
 import { getToken } from "services/Token";
 
-const socketUrl = `ws://${window.location.host}/log`;
-const token = getToken();
-
-export const useLogs = () => useWebSocket(socketUrl, {
-  queryParams: {
-    "Token": token ? token : ""
-  }
-});
+export const useLogs = () => {
+  const socketProtocol = window.location.protocol === "https:" ? "wss://" : "ws://";
+  const token = getToken() || "";
+  
+  return useWebSocket(
+    `${socketProtocol}${window.location.host}/log`,
+    {
+      queryParams: {
+        "Token": token,
+      },
+    },
+  );
+};
