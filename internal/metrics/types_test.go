9acc09f17274a0d5c6d82d6b7a538ab8a7f65a87
Pavlo Golub <pavlo.golub@gmail.com>
2025-04-10 16:22:00 +0200

    add measurements tests

--- internal/metrics/types_test.go
+++ internal/metrics/types_test.go
@@ -2,6 +2,7 @@ package metrics
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -37,4 +38,25 @@ func TestPrimaryOnly(t *testing.T) {
 	m.NodeStatus = "standby"
 	assert.False(t, m.PrimaryOnly())
 	assert.True(t, m.StandbyOnly())
+}
+
+func TestMeasurement(t *testing.T) {
+	m := NewMeasurement(1234567890)
+	assert.Equal(t, int64(1234567890), m.GetEpoch(), "epoch should be equal")
+	m[EpochColumnName] = "wrong type"
+	assert.True(t, time.Now().UnixNano()-m.GetEpoch() < int64(time.Second), "epoch should be close to now")
+}
+
+func TestMeasurements(t *testing.T) {
+	m := Measurements{}
+	assert.False(t, m.IsEpochSet(), "epoch should not be set")
+	assert.True(t, time.Now().UnixNano()-m.GetEpoch() < 100, "epoch should be close to now")
+	m = append(m, NewMeasurement(1234567890))
+	assert.True(t, m.IsEpochSet(), "epoch should be set")
+	assert.Equal(t, int64(1234567890), m.GetEpoch(), "epoch should be equal")
+	m1 := m.DeepCopy()
+	assert.Equal(t, m, m1, "deep copy should be equal")
+	m1.Touch()
+	assert.NotEqual(t, m, m1, "deep copy should be different")
+	assert.True(t, time.Now().UnixNano()-m1.GetEpoch() < int64(time.Second), "epoch should be close to now")
 }
