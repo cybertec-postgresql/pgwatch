20fd52ce167f03d6649f7d144b29b8a65e8f7c68
Pavlo Golub <pavlo.golub@gmail.com>
2025-04-30 20:35:41 +0200

    [*] refactor ResolveDatabasesFromPatroni by ResolveDatabasesFromPostgres

--- internal/sources/resolver.go
+++ internal/sources/resolver.go
@@ -12,10 +12,8 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"net/url"
 	"os"
 	"path"
-	"regexp"
 	"strings"
 	"time"
 
@@ -69,16 +67,6 @@ func getConsulClusterMembers(Source) ([]PatroniClusterMember, error) {
 
 func getZookeeperClusterMembers(Source) ([]PatroniClusterMember, error) {
 	return nil, errors.ErrUnsupported
-}
-
-func parseHostAndPortFromJdbcConnStr(connStr string) (string, string, error) {
-	r := regexp.MustCompile(`postgres://(.*)+:([0-9]+)/`)
-	matches := r.FindStringSubmatch(connStr)
-	if len(matches) != 3 {
-		logger.Errorf("Unexpected regex result groups:", matches)
-		return "", "", fmt.Errorf("unexpected regex result groups: %v", matches)
-	}
-	return matches[1], matches[2], nil
 }
 
 func jsonTextToStringMap(jsonText string) (map[string]string, error) {
@@ -168,25 +156,20 @@ func getEtcdClusterMembers(s Source) ([]PatroniClusterMember, error) {
 	kapi := c.KV
 
 	if s.Kind == SourcePatroniNamespace { // all scopes, all DBs (regex filtering applies if defined)
-		if len(s.GetDatabaseName()) > 0 {
-			return ret, fmt.Errorf("skipping Patroni entry %s - cannot specify a DB name when monitoring all scopes (regex patterns are supported though)", s.Name)
-		}
-		if s.HostConfig.Namespace == "" {
-			return ret, fmt.Errorf("skipping Patroni entry %s - search 'namespace' not specified", s.Name)
-		}
-		resp, err := kapi.Get(ctx, s.HostConfig.Namespace, client.WithPrefix(), client.WithKeysOnly())
+		namespace := cmp.Or(s.HostConfig.Namespace, "/service")
+		resp, err := kapi.Get(ctx, namespace, client.WithPrefix(), client.WithKeysOnly())
 		if err != nil {
 			return ret, cmp.Or(context.Cause(ctx), err)
 		}
 
 		// etcd3 does not have a dir node.
-		// Key="/service/batman/leader"
-		// Key="/service/batman/members/node"
+		// Key="/namespace/scope/leader", e.g. "/service/batman/leader"
+		// Key="/namespace/scope/members/node", e.g. "/service/batman/members/pg1"
 		//
 		// create unique map of first level items without Namespace
 		scopes := make(map[string]bool, len(resp.Kvs))
 		for _, node := range resp.Kvs {
-			pathSuffix := strings.TrimPrefix(string(node.Key), s.HostConfig.Namespace)
+			pathSuffix := strings.TrimPrefix(string(node.Key), namespace)
 			scope := strings.SplitN(pathSuffix, "/", 2)[0]
 			scopes[scope] = true
 		}
@@ -211,16 +194,15 @@ func getEtcdClusterMembers(s Source) ([]PatroniClusterMember, error) {
 func extractEtcdScopeMembers(ctx context.Context, s Source, scope string, kapi client.KV, addScopeToName bool) ([]PatroniClusterMember, error) {
 	var ret = make([]PatroniClusterMember, 0)
 	var name string
-	membersPath := path.Join(s.HostConfig.Namespace, scope, "members")
+	namespace := cmp.Or(s.HostConfig.Namespace, "/service")
+	membersPath := path.Join(namespace, scope, "members")
 
 	resp, err := kapi.Get(ctx, membersPath, client.WithPrefix())
 	if err != nil {
 		return nil, err
 	}
-	logger.Debugf("ETCD response for %s scope %s: %+v", s.Name, scope, resp)
 
 	for _, node := range resp.Kvs {
-		logger.Debugf("Found a cluster member from etcd [%s:%s]: %+v", s.Name, scope, node.Value)
 		nodeData, err := jsonTextToStringMap(string(node.Value))
 		if err != nil {
 			logger.Errorf("Could not parse ETCD node data for node \"%s\": %s", node, err)
@@ -245,103 +227,52 @@ const (
 	dcsTypeConsul    = "consul"
 )
 
-func ResolveDatabasesFromPatroni(ce Source) (SourceConns, error) {
+func ResolveDatabasesFromPatroni(source Source) (SourceConns, error) {
 	var mds []*SourceConn
 	var clusterMembers []PatroniClusterMember
 	var err error
 	var ok bool
-	var dbUnique string
 
-	switch ce.HostConfig.DcsType {
+	switch source.HostConfig.DcsType {
 	case dcsTypeEtcd:
-		clusterMembers, err = getEtcdClusterMembers(ce)
+		clusterMembers, err = getEtcdClusterMembers(source)
 	case dcsTypeZookeeper:
-		clusterMembers, err = getZookeeperClusterMembers(ce)
+		clusterMembers, err = getZookeeperClusterMembers(source)
 	case dcsTypeConsul:
-		clusterMembers, err = getConsulClusterMembers(ce)
+		clusterMembers, err = getConsulClusterMembers(source)
 	default:
 		return nil, errors.New("unknown DCS")
 	}
+	logger := logger.WithField("sorce", source.Name)
 	if err != nil {
-		logger.WithField("source", ce.Name).Debug("Failed to get info from DCS, using previous member info if any")
-		if clusterMembers, ok = lastFoundClusterMembers[ce.Name]; ok { // mask error from main loop not to remove monitored DBs due to "jitter"
+		logger.Debug("Failed to get info from DCS, using previous member info if any")
+		if clusterMembers, ok = lastFoundClusterMembers[source.Name]; ok { // mask error from main loop not to remove monitored DBs due to "jitter"
 			err = nil
 		}
 	} else {
-		lastFoundClusterMembers[ce.Name] = clusterMembers
+		lastFoundClusterMembers[source.Name] = clusterMembers
 	}
 	if len(clusterMembers) == 0 {
 		return mds, err
 	}
 
-	for _, m := range clusterMembers {
-		logger.Infof("Processing Patroni cluster member [%s:%s]", ce.Name, m.Name)
-		if ce.OnlyIfMaster && m.Role != "master" {
-			logger.Infof("Skipping over Patroni cluster member [%s:%s] as not a master", ce.Name, m.Name)
+	for _, patroniMember := range clusterMembers {
+		logger.Info("Processing Patroni cluster member: ", patroniMember.Name)
+		if source.OnlyIfMaster && patroniMember.Role != "master" {
 			continue
 		}
-		host, port, err := parseHostAndPortFromJdbcConnStr(m.ConnURL)
-		if err != nil {
-			logger.Errorf("Could not parse Patroni conn str \"%s\" [%s:%s]: %v", m.ConnURL, ce.Name, m.Scope, err)
-			continue
+		src := *source.Clone()
+		src.ConnStr = patroniMember.ConnURL
+		if source.Kind == SourcePatroniNamespace {
+			src.Name += "_" + patroniMember.Scope
 		}
-		if ce.OnlyIfMaster {
-			dbUnique = ce.Name
-			if ce.Kind == SourcePatroniNamespace {
-				dbUnique = ce.Name + "_" + m.Scope
-			}
+		src.Name += "_" + patroniMember.Name
+		if dbs, err := ResolveDatabasesFromPostgres(src); err == nil {
+			mds = append(mds, dbs...)
 		} else {
-			dbUnique = ce.Name + "_" + m.Name
+			logger.WithError(err).Error("Failed to resolve databases for Patroni member: ", patroniMember.Name)
 		}
-		if ce.GetDatabaseName() != "" {
-			c := &SourceConn{Source: *ce.Clone()}
-			c.Name = dbUnique
-			mds = append(mds, c)
-			continue
-		}
-		connURL, err := url.Parse(ce.ConnStr)
-		if err != nil {
-			logger.Errorf("Could not contact Patroni member [%s:%s]: %v", ce.Name, m.Scope, err)
-			continue
-		}
-		connURL.Scheme = "postgresql"
-		connURL.Host = host + ":" + port
-		connURL.Path = "template1"
-		c, err := db.New(context.TODO(), connURL.String())
-		if err != nil {
-			logger.Errorf("Could not contact Patroni member [%s:%s]: %v", ce.Name, m.Scope, err)
-			continue
-		}
-		defer c.Close()
-		sql := `select datname::text as datname,
-					quote_ident(datname)::text as datname_escaped
-					from pg_database
-					where not datistemplate
-					and datallowconn
-					and has_database_privilege (datname, 'CONNECT')
-					and case when length(trim($1)) > 0 then datname ~ $1 else true end
-					and case when length(trim($2)) > 0 then not datname ~ $2 else true end`
-
-		rows, err := c.Query(context.TODO(), sql, ce.IncludePattern, ce.ExcludePattern)
-		if err != nil {
-			return nil, err
-		}
-		data, err := pgx.CollectRows(rows, pgx.RowToMap)
-		if err != nil {
-			logger.Errorf("Could not get DB name listing from Patroni member [%s:%s]: %v", ce.Name, m.Scope, err)
-			continue
-		}
-
-		for _, d := range data {
-			connURL.Path = d["datname"].(string)
-			c := ce.Clone()
-			c.Name = dbUnique + "_" + d["datname_escaped"].(string)
-			c.ConnStr = connURL.String()
-			mds = append(mds, &SourceConn{Source: *c})
-		}
-
 	}
-
 	return mds, err
 }
 
--- internal/sources/resolver_test.go
+++ internal/sources/resolver_test.go
@@ -1,11 +1,15 @@
 package sources_test
 
 import (
+	"context"
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	client "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/server/v3/embed"
 
 	"github.com/cybertec-postgresql/pgwatch/v3/internal/sources"
 	testcontainers "github.com/testcontainers/testcontainers-go"
@@ -45,4 +49,71 @@ func TestMonitoredDatabase_ResolveDatabasesFromPostgres(t *testing.T) {
 	//check unexpected database
 	db = dbs.GetMonitoredDatabase(md.Name + "_unexpected")
 	assert.Nil(t, db)
+}
+
+func TestMonitoredDatabase_ResolveDatabasesFromPatroni(t *testing.T) {
+	// Start embedded etcd server
+	cfg := embed.NewConfig()
+	cfg.Dir = t.TempDir()
+	cfg.LogLevel = "error"
+	e, err := embed.StartEtcd(cfg)
+	require.NoError(t, err)
+	defer e.Close()
+
+	select {
+	case <-e.Server.ReadyNotify():
+		// ready
+	case <-time.After(5 * time.Second):
+		t.Fatal("etcd server took too long to start")
+	}
+
+	endpoint := e.Clients[0].Addr().String()
+
+	cli, err := client.New(client.Config{
+		Endpoints:   []string{"http://" + endpoint},
+		DialTimeout: 2 * time.Second,
+	})
+	require.NoError(t, err, "failed to create etcd client")
+	defer cli.Close()
+
+	// Start postgres server for testing
+	pgContainer, err := postgres.Run(ctx,
+		ImageName,
+		postgres.WithDatabase("mydatabase"),
+		postgres.WithInitScripts("../../docker/bootstrap/create_role_db.sql"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(5*time.Second)),
+	)
+	require.NoError(t, err)
+	defer func() { assert.NoError(t, pgContainer.Terminate(ctx)) }()
+
+	// Put values to etcd server
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	connStr, err := pgContainer.ConnectionString(ctx)
+	require.NoError(t, err)
+	_, err = cli.Put(ctx, "/service/batman/members/pg1",
+		fmt.Sprintf(`{"role":"master","conn_url":"%s"}`, connStr))
+	require.NoError(t, err)
+	_, err = cli.Put(ctx, "/service/batman/members/pg2",
+		`{"role":"standby","conn_url":"must_be_skipped"}`)
+	cancel()
+	require.NoError(t, err)
+
+	// Set up Source to use embedded etcd
+	md := sources.Source{}
+	md.Name = "continuous"
+	md.Kind = sources.SourcePatroni
+	md.OnlyIfMaster = true
+	md.HostConfig.DcsType = "etcd"
+	md.HostConfig.DcsEndpoints = []string{"http://" + endpoint}
+	md.HostConfig.Scope = "/batman/"
+	md.HostConfig.Namespace = "/service"
+
+	// Run ResolveDatabasesFromPatroni
+	dbs, err := sources.ResolveDatabasesFromPatroni(md)
+	assert.NoError(t, err)
+	assert.NotNil(t, dbs)
+	assert.Len(t, dbs, 4) // postgres, mydatrabase, pgwatch, pgwatch_metrics
 }
