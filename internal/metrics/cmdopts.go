4e1c4cf57b9aebbc8d3d77d16537603bf6f52458
Pavlo Golub <pavlo.golub@gmail.com>
2025-05-12 13:16:16 +0200

    [-] fix `--metrics` command-option description, fixes #756 (#757)

--- internal/metrics/cmdopts.go
+++ internal/metrics/cmdopts.go
@@ -6,7 +6,7 @@ import (
 
 // CmdOpts specifies metric command-line options
 type CmdOpts struct {
-	Metrics                      string `short:"m" long:"metrics" mapstructure:"metrics" description:"Postgres URI, file or folder of YAML files with metrics definitions" env:"PW_METRICS"`
+	Metrics                      string `short:"m" long:"metrics" mapstructure:"metrics" description:"Postgres URI or path to YAML file with metrics definitions" env:"PW_METRICS"`
 	DirectOSStats                bool   `long:"direct-os-stats" mapstructure:"direct-os-stats" description:"Extract OS related psutil statistics not via PL/Python wrappers but directly on host" env:"PW_DIRECT_OS_STATS"`
 	InstanceLevelCacheMaxSeconds int64  `long:"instance-level-cache-max-seconds" mapstructure:"instance-level-cache-max-seconds" description:"Max allowed staleness for instance level metric data shared between DBs of an instance. Set to 0 to disable" env:"PW_INSTANCE_LEVEL_CACHE_MAX_SECONDS" default:"30"`
 	EmergencyPauseTriggerfile    string `long:"emergency-pause-triggerfile" mapstructure:"emergency-pause-triggerfile" description:"When the file exists no metrics will be temporarily fetched" env:"PW_EMERGENCY_PAUSE_TRIGGERFILE" default:"/tmp/pgwatch-emergency-pause"`
