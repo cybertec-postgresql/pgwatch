a10bdac61cc556928fdf6a62b09328d34bc77d34
Pavlo Golub <pavlo.golub@gmail.com>
2025-08-30 15:08:28 +0200

    [+] add "Tables Overview" dashboard (#918)
    * [+] add "Tables Overview" dashboard
    
    New dashboard uses tabs/rows and combines old "Tables Top" and "Biggest
    relations treemap" dashboards. Tweak "Table details" dashboard

--- docker/compose.pgbench.sh
+++ docker/compose.pgbench.sh
@@ -4,5 +4,5 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 docker compose exec -e PGDATABASE=pgwatch postgres sh -c \
  "pgbench --initialize --scale=50 &&
-  pgbench --progress=5 --client=10 --jobs=2 --transactions=10000 &&
+  pgbench --progress=5 --client=10 --jobs=2 --time=300 --rate=150 &&
   pgbench --initialize --init-steps=d"
