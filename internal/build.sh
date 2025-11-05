a848ef6c02d53bfa6d5a6ed61049dcf98be3638d
Pavlo Golub <pavlo.golub@gmail.com>
2025-11-05 12:02:42 +0100

    [+] switch to the Green Tea Garbage Collector (#1007)

--- internal/build.sh
+++ internal/build.sh
@@ -6,4 +6,4 @@ set -x
 GIT_HASH=$(git show -s --format=%H HEAD)
 GIT_TIME=$(git show -s --format=%cI HEAD)
 VERSION=$(git rev-parse --abbrev-ref HEAD)
-go build -ldflags "-X 'main.commit=$GIT_HASH' -X 'main.date=$GIT_TIME' -X 'main.version=$VERSION'"
+GOEXPERIMENT=greenteagc go build -ldflags "-X 'main.commit=$GIT_HASH' -X 'main.date=$GIT_TIME' -X 'main.version=$VERSION'"
