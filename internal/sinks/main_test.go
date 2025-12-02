321c1d485944b33ec8e3346ceef226f483318815
0xgouda <gouda0x@gmail.com>
2025-12-02 18:46:44 +0200

    Move pg container setup logic to `TestMain()`.

---
+++ internal/sinks/main_test.go
@@ -0,0 +1,34 @@
+package sinks
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/cybertec-postgresql/pgwatch/v3/internal/log"
+)
+
+var ctx = log.WithLogger(context.Background(), log.NewNoopLogger())
+
+func TestMain(m *testing.M) {
+	// Setup
+
+	err := RPCTestsSetup()
+	if err != nil {
+		panic(err)
+	}
+
+	err = PGTestsSetup()
+	if err != nil {
+		panic(err)
+	}	
+
+	// Execute all tests
+	exitCode := m.Run()
+
+	// Teardown
+	RPCTestsTeardown()
+	PGTestsTeardown()
+
+	os.Exit(exitCode)
+}
