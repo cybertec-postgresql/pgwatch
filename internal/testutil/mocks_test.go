527900138a2746577e249d3d595be2f24fab1c20
0xgouda <gouda0x@gmail.com>
2025-12-05 20:15:45 +0200

    Add tests for `Mock{Sources,Metrics}ReaderWriter` to increase test coverage.

--- internal/testutil/mocks_test.go
+++ internal/testutil/mocks_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 
 	"github.com/cybertec-postgresql/pgwatch/v3/api/pb"
+	"github.com/cybertec-postgresql/pgwatch/v3/internal/metrics"
+	"github.com/cybertec-postgresql/pgwatch/v3/internal/sources"
 	"github.com/cybertec-postgresql/pgwatch/v3/internal/testutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -112,5 +114,168 @@ func TestReceiver_DefineMetrics(t *testing.T) {
 		assert.Error(t, err)
 		assert.Nil(t, reply)
 		assert.EqualError(t, err, "empty metrics struct")
+	})
+}
+
+func TestMockMetricsReaderWriter(t *testing.T) {
+	testData := &metrics.Metrics{
+		MetricDefs: map[string]metrics.Metric{"foo": {Description: "bar"}},
+		PresetDefs: map[string]metrics.Preset{"foo": {Description: "bar"}},
+	}
+	called := false
+
+	mock := testutil.MockMetricsReaderWriter{
+		GetMetricsFunc: func() (*metrics.Metrics, error) {
+			called = true
+			return testData, nil
+		},
+		WriteMetricsFunc: func(*metrics.Metrics) error {
+			called = true
+			return nil
+		},
+		UpdateMetricFunc: func(string, metrics.Metric) error {
+			called = true
+			return nil
+		},
+		DeleteMetricFunc: func(string) error {
+			called = true
+			return nil
+		},
+		CreateMetricFunc: func(string, metrics.Metric) error {
+			called = true
+			return nil
+		},
+		CreatePresetFunc: func(string, metrics.Preset) error {
+			called = true
+			return nil
+		},
+		UpdatePresetFunc: func(string, metrics.Preset) error {
+			called = true
+			return nil
+		},
+		DeletePresetFunc: func(string) error {
+			called = true
+			return nil
+		},
+	}
+
+	t.Run("GetMetrics", func(t *testing.T) {
+		called = false
+		metrics, err := mock.GetMetrics()
+		assert.NoError(t, err)
+		assert.Equal(t, true, called)
+		assert.Equal(t, testData, metrics)
+	})
+
+	t.Run("WriteMetrics", func(t *testing.T) {
+		called = false
+		err := mock.WriteMetrics(testData)
+		assert.NoError(t, err)
+		assert.Equal(t, true, called)
+	})
+
+	t.Run("UpdateMetric", func(t *testing.T) {
+		called = false
+		err := mock.UpdateMetric("foo", testData.MetricDefs["foo"])
+		assert.NoError(t, err)
+		assert.Equal(t, true, called)
+	})
+
+	t.Run("DeleteMetric", func(t *testing.T) {
+		called = false
+		err := mock.DeleteMetric("foo")
+		assert.NoError(t, err)
+		assert.Equal(t, true, called)
+	})
+
+	t.Run("CreateMetric", func(t *testing.T) {
+		called = false
+		err := mock.CreateMetric("foo", testData.MetricDefs["foo"])
+		assert.NoError(t, err)
+		assert.Equal(t, true, called)
+	})
+
+	t.Run("CreatePreset", func(t *testing.T) {
+		called = false
+		err := mock.CreatePreset("foo", testData.PresetDefs["foo"])
+		assert.NoError(t, err)
+		assert.Equal(t, true, called)
+	})
+
+	t.Run("UpdatePreset", func(t *testing.T) {
+		called = false
+		err := mock.UpdatePreset("foo", testData.PresetDefs["foo"])
+		assert.NoError(t, err)
+		assert.Equal(t, true, called)
+	})
+
+	t.Run("DeletePreset", func(t *testing.T) {
+		called = false
+		err := mock.DeletePreset("foo")
+		assert.NoError(t, err)
+		assert.Equal(t, true, called)
+	})
+}
+
+func TestMockSourcesReaderWriter(t *testing.T) {
+	testData := sources.Sources{{Name: "foo", ConnStr: "postgres://foo@bar"}}
+	called := false
+	mock := testutil.MockSourcesReaderWriter{
+		GetSourcesFunc: func() (sources.Sources, error) {
+			called = true
+			return testData, nil
+		},
+		WriteSourcesFunc: func(sources.Sources) error {
+			called = true
+			return nil
+		},
+		UpdateSourceFunc: func(sources.Source) error {
+			called = true
+			return nil
+		},
+		CreateSourceFunc: func(sources.Source) error {
+			called = true
+			return nil
+		},
+		DeleteSourceFunc: func(string) error {
+			called = true
+			return nil
+		},
+	}
+
+	t.Run("GetSources", func(t *testing.T) {
+		called = false
+		sources, err := mock.GetSources()
+		assert.NoError(t, err)
+		assert.Equal(t, true, called)
+		assert.Equal(t, testData, sources)
+	})
+
+	t.Run("WriteSources", func(t *testing.T) {
+		called = false
+		err := mock.WriteSources(testData)
+		assert.NoError(t, err)
+		assert.Equal(t, true, called)
+	})
+
+	t.Run("UpdateSource", func(t *testing.T) {
+		called = false
+		err := mock.UpdateSource(testData[0])
+		assert.NoError(t, err)
+		assert.Equal(t, true, called)
+	})
+
+	t.Run("DeleteSource", func(t *testing.T) {
+		called = false
+		err := mock.DeleteSource("foo")
+		assert.NoError(t, err)
+		assert.Equal(t, true, called)
+	})
+
+	t.Run("CreateSource", func(t *testing.T) {
+		called = false
+		err := mock.CreateSource(testData[0])
+		assert.NoError(t, err)
+		assert.Equal(t, true, called)
 	})
 }
