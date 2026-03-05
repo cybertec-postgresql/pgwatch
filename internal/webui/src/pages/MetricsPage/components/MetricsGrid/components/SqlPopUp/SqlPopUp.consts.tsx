00a10daa54ab1ee3586efafe3444558db7ddae99
Mohamed Elfardy <mohamedyaserelfardy18@gmail.com>
2026-03-05 23:39:30 +0200

    make The metric sqls in webui readable

--- internal/webui/src/pages/MetricsPage/components/MetricsGrid/components/SqlPopUp/SqlPopUp.consts.tsx
+++ internal/webui/src/pages/MetricsPage/components/MetricsGrid/components/SqlPopUp/SqlPopUp.consts.tsx
@@ -1,15 +1,35 @@
 import { GridColDef } from "@mui/x-data-grid";
+import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
+import sql from "react-syntax-highlighter/dist/esm/languages/hljs/sql";
+import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+SyntaxHighlighter.registerLanguage("sql", sql);
 
 export const useSqlPopUpColumns = (): GridColDef[] => [
   {
     field: "version",
     headerName: "Version",
+    width: 80,
   },
   {
     field: "sql",
     headerName: "SQL",
-    align: "center",
-    headerAlign: "center",
-    width: 600,
+    headerAlign: "left",
+    flex: 1,
+    renderCell: (params) => (
+      <SyntaxHighlighter
+        language="sql"
+        style={atomOneDark}
+        wrapLongLines={true}
+        customStyle={{
+          margin: 0,
+          fontSize: "12px",
+          borderRadius: "4px",
+          width: "100%",
+        }}
+      >
+        {params.value}
+      </SyntaxHighlighter>
+    ),
   },
 ];
