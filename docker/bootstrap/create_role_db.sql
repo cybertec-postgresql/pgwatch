ffff1125e47f04f47b742ad2c8148ae7384f04f8
0xgouda <gouda0x@gmail.com>
2026-03-11 16:46:14 +0200

    Update postgres docker images to enable `logging_collector`
    - Also use `csvlog` as the log destination
    - Grant execute privilege on `pg_read_file(text, bigint, bigint)` for `pgwatch` user

--- docker/bootstrap/create_role_db.sql
+++ docker/bootstrap/create_role_db.sql
@@ -5,3 +5,6 @@ CREATE ROLE pgwatch WITH
 CREATE DATABASE pgwatch OWNER pgwatch;
 
 CREATE DATABASE pgwatch_metrics OWNER pgwatch;
+
+-- for remote log parsing to work in case of server_log_event_counts metric is used
+GRANT EXECUTE ON FUNCTION pg_read_file(text, bigint, bigint) TO pgwatch;
