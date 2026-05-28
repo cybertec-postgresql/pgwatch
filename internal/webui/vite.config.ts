eb3d3ce0dffa6969c03cc484a17118a4667c0881
Pavlo Golub <pavlo.golub@gmail.com>
2026-05-28 06:32:13 -0700

    [+] bump `Vite` from 7.3.2 to 8.0.14 (#1421)

--- internal/webui/vite.config.ts
+++ internal/webui/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
 
 const handleGoTemplates = (command: string) => ({
@@ -21,11 +20,13 @@ export default defineConfig(({ command }) => ({
   plugins: [
     handleGoTemplates(command),
     react(),
-    tsconfigPaths(),
     svgr({
       include: '**/*.svg',
     }),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
   base: './',
   server: {
     port: 4000,
