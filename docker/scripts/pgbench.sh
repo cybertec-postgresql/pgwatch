7232eff0b653d8fd2ea778ab238d56b952344fb0
Pavlo Golub <pavlo.golub@gmail.com>
2026-08-09 23:25:21 +0200

    [+] add support for Task build tool

--- docker/scripts/pgbench.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "${BASH_SOURCE[0]}")/.."
-
-docker compose exec -e PGDATABASE=pgwatch postgres sh -c \
- "pgbench --initialize --scale=50 &&
-  pgbench --progress=5 --client=10 --jobs=2 --time=300 --rate=150 &&
-  pgbench --initialize --init-steps=d"
